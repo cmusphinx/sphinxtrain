b0d75d8653303bdecb623974bc0a4f46b1badafc
egouvea <egouvea@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-11-30 06:26:49 +0000

    Undid last change
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@2256 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/libs/libio/s3io.c
+++ src/libs/libio/s3io.c
@@ -308,7 +308,7 @@ error_loc:
 }
 
 size_t
-s3read(char *pointer,
+s3read(void *pointer,
        size_t size,
        size_t num_items,
        FILE *stream,
@@ -618,7 +618,7 @@ s3read_1d(void **arr,
 }
 
 size_t
-s3write(const char *pointer,
+s3write(const void *pointer,
 	size_t size,
 	size_t num_items,
 	FILE *stream,
@@ -808,8 +808,8 @@ s3write_1d(void *arr,
  * Log record.  Maintained by RCS.
  *
  * $Log$
- * Revision 1.5  2003/11/30  05:57:05  egouvea
- * Replaced 'void *' with 'char *', so the pointer points to something with known size
+ * Revision 1.6  2003/11/30  06:26:49  egouvea
+ *  Undid last change
  * 
  * Revision 1.4  2001/04/05 20:02:31  awb
  * *** empty log message ***
