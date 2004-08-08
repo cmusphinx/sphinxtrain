219341fef05355c24ddfab769c85d88785bb99e9
arthchan2003 <arthchan2003@94700074-3cef-4d97-a70e-9c8c206c02f5>
2004-08-08 04:30:56 +0000

    mk_flat help and example
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@3645 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/programs/mk_flat/parse_cmd_ln.c
+++ src/programs/mk_flat/parse_cmd_ln.c
@@ -47,6 +47,10 @@
 
 #include <s3/cmd_ln.h>
 
+#include <s3/s3.h>
+
+#include <sys_compat/misc.h>
+
 #include "parse_cmd_ln.h"
 
 #include <stdlib.h>
@@ -54,7 +58,40 @@
 int
 parse_cmd_ln(int argc, char *argv[])
 {
+  uint32      isHelp;
+  uint32      isExample;
+
+  const char helpstr[]=
+"Description:\n\
+(Copied from Rita's web page) \n\
+In flat-initialization, all mixture weights are set to be equal for \n\
+all states, and all state transition probabilities are set to be \n\
+equal. Unlike in continuous models, the means and variances of the \n\
+codebook Gaussians are not given global values, since they are already \n\
+estimated from the data in the vector quantization step. To \n\
+flat-initialize the mixture weights, each component of each \n\
+mixture-weight distribution of each feature stream is set to be a \n\
+number equal to 1/N, where N is the codebook size. The mixture_weights \n\
+and transition_matrices are initialized using the executable mk_flat ";
+
+  const char examplestr[]=
+"Example:\n\
+\n\
+mk_flat -moddeffn CFS3.ci.mdef -topo CFS3.topology -mixwfn mixture_weights  \n\
+-tmatfn transition_matrices -nstream 1 -ndensity 1 ";
     static arg_def_t defn[] = {
+	{ "-help",
+	  CMD_LN_BOOLEAN,
+	  CMD_LN_NO_VALIDATION,
+	  "no",
+	  "Shows the usage of the tool"},
+
+	{ "-example",
+	  CMD_LN_BOOLEAN,
+	  CMD_LN_NO_VALIDATION,
+	  "no",
+	  "Shows example of how to use the tool"},
+
 	{ "-moddeffn",
 	      CMD_LN_STRING,
 	      CMD_LN_NO_VALIDATION,
@@ -101,7 +138,25 @@ parse_cmd_ln(int argc, char *argv[])
 
     cmd_ln_parse(argc, argv);
 
-    cmd_ln_print_configuration();
+    isHelp    = *(uint32 *) cmd_ln_access("-help");
+    isExample    = *(uint32 *) cmd_ln_access("-example");
+
+
+    if(isHelp){
+      printf("%s\n\n",helpstr);
+    }
+
+    if(isExample){
+      printf("%s\n\n",examplestr);
+    }
+
+    if(isHelp || isExample){
+      E_FATAL("User ask for help or example, stop before proceed\n");
+    }
+    if(!isHelp && !isExample){
+      cmd_ln_print_configuration();
+    }
+
     return 0;
 }
 
@@ -109,9 +164,12 @@ parse_cmd_ln(int argc, char *argv[])
  * Log record.  Maintained by RCS.
  *
  * $Log$
- * Revision 1.4  2004/07/21  18:30:35  egouvea
- * Changed the license terms to make it the same as sphinx2 and sphinx3.
+ * Revision 1.5  2004/08/08  04:30:56  arthchan2003
+ * mk_flat help and example
  * 
+ * Revision 1.4  2004/07/21 18:30:35  egouvea
+ * Changed the license terms to make it the same as sphinx2 and sphinx3.
+ *
  * Revision 1.3  2001/04/05 20:02:31  awb
  * *** empty log message ***
  *
