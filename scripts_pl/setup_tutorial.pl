653829036ce23779afb69a91c5d1273b2b941b19
egouvea <egouvea@94700074-3cef-4d97-a70e-9c8c206c02f5>
2005-04-27 18:51:55 +0000

    Added script to setup the tutorial, added missing fiels to Makefiles, fixed minor changes in tinydoc.txt
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@4417 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ scripts_pl/setup_tutorial.pl
@@ -0,0 +1,36 @@
+use strict;
+use Cwd;
+use File::Path;
+
+my $SphinxTrain_dir = getcwd();
+
+my $task = "an4";
+if ($#ARGV > -1) {
+  $task = $ARGV[0];
+}
+
+print "Building task $task\n";
+
+my $task_dir = "../$task";
+chdir "$task_dir";
+
+system("perl \"$SphinxTrain_dir/scripts_pl/setup_SphinxTrain.pl\" " .
+       "-force -sphinxtraindir \"$SphinxTrain_dir\" -task $task");
+
+mkdir "feat" if (! -e "feat");
+open (CTL, "etc/${task}_train.fileids") 
+  or die "Could not open control file etc/${task}_train.fileids\n";
+while (<CTL>) {
+  s/[\/\\][^\/\\]+//g;
+  mkpath ("feat/$_", 0, 0755) if (! -e "feat/$_");
+}
+close(CTL);
+
+print "\n\nNow, please do:\n";
+print "\tcd $task_dir\n";
+print "And then, in Unix/Linux:\n";
+print "\tperl scripts_pl/make_feats.pl\n";
+print "\tperl scripts_pl/RunAll.pl\n";
+print "Or in Windows:\n";
+print "\tperl scripts_pl\\make_feats.pl\n";
+print "\tperl scripts_pl\\RunAll.pl\n";
