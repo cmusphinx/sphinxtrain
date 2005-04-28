d431015b8b1f952fe355cd012defc39576b41488
egouvea <egouvea@94700074-3cef-4d97-a70e-9c8c206c02f5>
2005-04-28 00:59:39 +0000

    Fixed previous checkin which removed more lines than it should
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@4419 94700074-3cef-4d97-a70e-9c8c206c02f5

--- scripts_pl/setup_SphinxTrain.pl
+++ scripts_pl/setup_SphinxTrain.pl
@@ -157,6 +157,7 @@ foreach my $executable (@dirlist) {
 mkdir "scripts_pl" unless -d scripts_pl;
 my $scriptdir = "$SPHINXTRAINDIR/scripts_pl";
 print "Copying scripts from $scriptdir\n";
+opendir(DIR, "$scriptdir") or die "Can't open $scriptdir\n";
 @dirlist = grep /^(0.*|mc)$/, readdir DIR;
 closedir(DIR);
 
