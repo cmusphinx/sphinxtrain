8cd20926691d34da1609361d61a9696c7a0904a2
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2006-08-08 13:44:12 +0000

    Add a test of LDA with init_gau
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@6015 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ test/scripts/test_init_gau_lda.pl
@@ -0,0 +1,38 @@
+#!/usr/local/bin/perl
+
+use strict;
+use File::Copy;
+require './scripts/testlib.pl';
+
+my $bindir="../bin.i686-pc-linux-gnu/";
+my $resdir="res/";
+my $exec_resdir="init_gau";
+my $bin="$bindir$exec_resdir";
+my $bin_printp="${bindir}printp";
+
+my $lda="./res/hmm/RM.lda";
+my $cepdir="./res/feat/rm";
+my $gd_cnt="./gauden_counts";
+my $gd_out="./gd_cnt.out";
+
+my $cmd="$bin ";
+$cmd .= "-part 1 -npart 1 ";
+$cmd .= "-cepdir $cepdir ";
+$cmd .= "-cepext mfc -accumdir ./ ";
+$cmd .= "-agc none -cmn current ";
+$cmd .= "-ldafn $lda -ldadim 29 ";
+$cmd .= "-varnorm no -feat 1s_c_d_dd -ceplen 13 ";
+
+test_help($bindir,$exec_resdir);
+test_example($bindir,$exec_resdir);
+
+my $ctlfn="./res/feat/rm/rm1_train.fileids.100";
+my $matchfn="./$exec_resdir/test_gauden_counts_lda.100.out";
+$cmd .= "-ctlfn $ctlfn ";
+
+test_this("$cmd",$exec_resdir,"DRY RUN TEST for 100 utterances");
+test_this("$bin_printp -gaucntfn $gd_cnt > $gd_out ",$exec_resdir,"printp gau count for 100 utterances");
+compare_these_two($gd_out,$matchfn,$exec_resdir,"LDA Gau count generation for 100 utterances");
+copy("./gauden_counts","./init_gau/gauden_counts_lda.100");
+
+unlink($gd_cnt,$gd_out);
