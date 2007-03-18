4b2dcb29de8b2d082d5127aee723e7108dd14809
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-03-18 19:48:09 +0000

    import the feature modules into feat, so you can access them as feat.s2_4x after doing from sphinx import feat
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@6549 94700074-3cef-4d97-a70e-9c8c206c02f5

--- python/sphinx/feat/__init__.py
+++ python/sphinx/feat/__init__.py
@@ -5,3 +5,6 @@ SphinxTrain Python Modules for feature computation.
 __all__ = ['_1s_c_d_dd', 's2_4x']
 __author__ = "David Huggins-Daines <dhuggins@cs.cmu.edu>"
 __version__ = "$Revision$"
+
+import _1s_c_d_dd
+import s2_4x
