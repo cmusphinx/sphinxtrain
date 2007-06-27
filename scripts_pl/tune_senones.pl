f12d70b214cab9702740c6a27538433776466273
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-06-27 17:24:00 +0000

    A script to generate a set of acoustic models with different numbers of tied states and decoding results for them
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@6810 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ scripts_pl/tune_senones.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Pod::Usage;
+use File::Spec::Functions;
+use File::Basename;
+use lib catdir(dirname($0), 'lib');
+use SphinxTrain::Config;
+use SphinxTrain::Util;
+
+pod2usage(1) unless @ARGV == 3;
+my ($start, $end, $incr) = @ARGV;
+
+my $orig_cfgfile = $ST::CFG_FILE;
+my $new_cfgfile = catdir($ST::CFG_BASE_DIR, 'tune_senones.cfg');
+my $orig_decode_cfgfile = catdir($ST::CFG_BASE_DIR, 'etc', 'sphinx_decode.cfg');
+my $new_decode_cfgfile = catdir($ST::CFG_BASE_DIR, 'tune_senones_decode.cfg');
+
+for (my $nsen = $start; $nsen <= $end; $nsen += $incr) {
+    # Build a temporary decode configuration
+    open CFGIN, "<$orig_decode_cfgfile" or die "Failed to open $orig_decode_cfgfile: $!";
+    open CFGOUT, ">$new_decode_cfgfile" or die "Failed to open $new_decode_cfgfile: $!";
+    while (<CFGIN>) {
+	s/\$DEC_CFG_SPHINXTRAIN_CFG\s*=\s*[^;]+;/\$DEC_CFG_SPHINXTRAIN_CFG = \"$new_cfgfile\";/;
+	print CFGOUT;
+    }
+    close CFGIN;
+    close CFGOUT;
+    # And a temporary training configuration
+    open CFGIN, "<$orig_cfgfile" or die "Failed to open $orig_cfgfile: $!";
+    open CFGOUT, ">$new_cfgfile" or die "Failed to open $new_cfgfile: $!";
+    while (<CFGIN>) {
+	s/\$CFG_N_TIED_STATES\s*=\s*[^;]+;/\$CFG_N_TIED_STATES = $nsen;/;
+	print CFGOUT;
+    }
+    close CFGIN;
+    close CFGOUT;
+
+    my $rv;
+    # We set this so that RunScript() will pass it on to sub-scripts
+    $ST::CFG_FILE = $new_cfgfile;
+    $rv = RunScript(catfile($ST::CFG_SCRIPT_DIR, '40.buildtrees', 'prunetree.pl'), $nsen);
+    die "prunetree.pl failed: $rv" if $rv;
+    $rv = RunScript(catfile($ST::CFG_SCRIPT_DIR, '40.buildtrees', 'tiestate.pl'), $nsen);
+    die "tiestate.pl failed: $rv" if $rv;
+    $rv = RunScript(catfile($ST::CFG_SCRIPT_DIR, '50.cd_hmm_tied', 'slave_convg.pl'));
+    die "cd_hmm_tied training failed: $rv" if $rv;
+
+    $ST::CFG_FILE = $new_decode_cfgfile;
+    $rv = RunScript(catfile($ST::CFG_SCRIPT_DIR, 'decode', 'slave.pl'));
+    die "decode failed: $rv" if $rv;
+
+    rename(catfile($ST::CFG_BASE_DIR, 'result', "$ST::CFG_EXPTNAME.match"),
+	   catfile($ST::CFG_BASE_DIR, 'result', "$ST::CFG_EXPTNAME-$nsen.match"));
+    rename(catfile($ST::CFG_BASE_DIR, 'result', "$ST::CFG_EXPTNAME.align"),
+	   catfile($ST::CFG_BASE_DIR, 'result', "$ST::CFG_EXPTNAME-$nsen.align"));
+}
+
+unlink $new_cfgfile;
+unlink $new_decode_cfgfile;
+
+1;
+__END__
+
+=head1 NAME
+
+tune_senones.pl
+
+=head1 SYNOPSIS
+
+tune_senones.pl I<start> I<end> I<increment>
+
+=head1 DESCRIPTION
+
+This script generates acoustic models with different numbers of tied
+states in the range from I<start> to I<end> in steps of I<increment>,
+and runs decoding tests on each of them.
+
+=head1 AUTHOR
+
+David Huggins-Daines <dhuggins@cs.cmu.edu>
+
+=cut
