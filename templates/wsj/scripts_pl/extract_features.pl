b72fbc3105163122b3f90fe2a985c232cdc5c258
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-06-28 02:20:09 +0000

    Extract some or all WSJ features
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@6814 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ templates/wsj/scripts_pl/extract_features.pl
@@ -0,0 +1,116 @@
+#!/usr/bin/perl -w
+use strict;
+use Getopt::Long;
+use Pod::Usage;
+use File::Spec::Functions qw(catdir catfile rel2abs);
+use File::Basename;
+use File::Temp qw(tempfile);
+use File::Find;
+
+use lib catdir(dirname($0), 'lib');
+use SimpleConfig;
+
+my %opts = (config => 'etc/wsj_files.cfg',
+	    outdir => 'feat',
+	    parts => 'si84,devtest');
+GetOptions(\%opts,
+	   'config=s',
+	   'parts=s',
+	   'outdir|o=s',
+	   'help|h|?')
+    or pod2usage(2);
+pod2usage(0) if $opts{help};
+
+my %dirs = parse_config($opts{config});
+my @parts = split /,/, $opts{parts};
+my @dirs;
+my %dirmap = (si84 => ['wsj0_si_tr_s'],
+	      si200 => ['wsj1_si_tr_s'],
+	      extra => ['wsj0_sd_tr_l',
+			'wsj0_sd_tr_s',
+			'wsj1_si_tr_j',
+			'wsj1_si_tr_jd',
+			'wsj1_si_tr_l'],
+	      devtest => ['wsj0_si_dt_05',
+			  'wsj0_si_dt_20',
+			  'wsj0_si_et_05',
+			  'wsj0_si_et_20'],
+	     );
+
+foreach my $p (@parts) {
+    push @dirs, @{$dirmap{$p}};
+}
+
+foreach my $dir (@dirs) {
+    my $indir = $dirs{$dir};
+    my ($od) = ($dir =~ /^wsj[01]_(.*)/);
+    my $outdir = rel2abs(catdir($opts{outdir}, $od));
+    mkdir($opts{outdir}, 0777);
+    mkdir($outdir, 0777);
+    my @files;
+    find(sub {
+	     my $relpath = $File::Find::name;
+	     substr($relpath, 0, length($indir)) = "";
+	     $relpath =~ s/^\/+//;
+
+	     if (-f $File::Find::name and $File::Find::name =~ /\.wv1/i) {
+		 push @files, $relpath;
+	     }
+	     elsif (-d $File::Find::name and !/^\.\.?$/) {
+		 mkdir(catdir($outdir, lc($relpath)), 0777);
+	     }
+	 }, $indir);
+    # Do them in batches of 50 to save disk space
+    while (@files) {
+	my @l = splice @files, 0, 50;
+	my ($tmpfh, $tmpfile) = tempfile();
+	foreach my $p (@l) {
+	    my $o = lc($p);
+	    $o =~ s/\.wv1$//;
+	    my $rv = system("sph2pipe",
+			    catfile($indir, $p),
+			    catfile($outdir, "$o.sph"));
+	    die "sph2pipe failed: $rv" unless $rv == 0;
+	    print $tmpfh "$o\n";
+	}
+	my $rv = system('sphinx_fe',
+			-di => $outdir, -do => $outdir,
+			-ei => 'sph', -eo => 'mfc',
+			-c => $tmpfile, -nist => 'yes');
+	foreach my $p (@l) {
+	    my $o = lc($p);
+	    $o =~ s/\.wv1$//;
+	    unlink(catfile($outdir, "$o.sph"));
+	}
+	unlink $tmpfile;
+    }
+}
+
+1;
+__END__
+
+=head1 NAME
+
+extract_features.pl - Create acoustic feature files for WSJ
+
+=head1 SYNOPSIS
+
+create_features.pl
+     S<[ I<--config> B<etc/rm1_files.cfg> ]>
+     S<[ I<--parts> B<si84,devtest> ]>
+     S<[ I<-o> B</path/to/training/feat> ]>
+
+=head1 DESCRIPTION
+
+By default, this script creates feature files for all waveforms in the
+WSJ SI-84 training set and the 5k and 20k development and testing
+sets.  Only the Sennheiser microphone (.wv1) files are used.
+Optionally, it will extract the SI-200 and extra training data as
+well.
+
+=head1 AUTHOR
+
+David Huggins-Daines <dhuggins@cs.cmu.edu>
+
+=cut
+
