c5a1d8eae468151c00042aa1cc8411cc088e1ca7
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-01-02 05:57:14 +0000

    Add LDA/MLLT training based on CI models.  It is inconclusive whether this works better or worse than using CD models.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@7537 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ scripts_pl/05.lda_train/baum_welch.pl
@@ -0,0 +1,162 @@
+#!/usr/bin/perl
+## ====================================================================
+##
+## Copyright (c) 1996-2000 Carnegie Mellon University.  All rights
+## reserved.
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions
+## are met:
+##
+## 1. Redistributions of source code must retain the above copyright
+##    notice, this list of conditions and the following disclaimer.
+##
+## 2. Redistributions in binary form must reproduce the above copyright
+##    notice, this list of conditions and the following disclaimer in
+##    the documentation and/or other materials provided with the
+##    distribution.
+##
+## This work was supported in part by funding from the Defense Advanced
+## Research Projects Agency and the National Science Foundation of the
+## United States of America, and the CMU Sphinx Speech Consortium.
+##
+## THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND
+## ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+## THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+## PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
+## NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+##
+## ====================================================================
+##
+## Author: Ricky Houghton
+## Author: David Huggins-Daines
+##
+
+use strict;
+use File::Copy;
+use File::Basename;
+use File::Spec::Functions;
+use File::Path;
+
+use lib catdir(dirname($0), updir(), 'lib');
+use SphinxTrain::Config;
+use SphinxTrain::Util;
+
+#************************************************************************
+# this script performs baum-welch training using
+# it needs as inputs an initial set of models in s3 format
+# a mdef file and cepstra with transcription files.
+#************************************************************************
+
+$| = 1; # Turn on autoflushing
+
+die "USAGE: $0 <iter> <part> <npart> <fullvar>" if (@ARGV != 4);
+my ($iter, $part, $npart, $fullvar) = @ARGV;
+
+# Accumulate over the CI models
+my $modelinitialname="${ST::CFG_EXPTNAME}.ci_${ST::CFG_DIRLABEL}_flatinitial";
+my $modelname="${ST::CFG_EXPTNAME}.ci_${ST::CFG_DIRLABEL}";
+my $mdefname="${ST::CFG_EXPTNAME}.ci.mdef";
+my $processname = "05.lda_train";
+
+my $output_buffer_dir = "$ST::CFG_BWACCUM_DIR/${ST::CFG_EXPTNAME}_buff_${part}";
+mkdir ($output_buffer_dir,0777);
+
+my ($hmm_dir, $var2pass);
+if ($iter == 1) {
+    $hmm_dir  = "$ST::CFG_BASE_DIR/model_parameters/$modelinitialname";
+    $var2pass	 = "no";
+} else {
+    $hmm_dir      = "$ST::CFG_BASE_DIR/model_parameters/$modelname";
+    $var2pass	  = "yes";
+}
+
+my $moddeffn    = "$ST::CFG_BASE_DIR/model_architecture/$mdefname";
+my $statepdeffn = $ST::CFG_HMM_TYPE; # indicates the type of HMMs
+my $mixwfn  = "$hmm_dir/mixture_weights";
+my $mwfloor = 1e-5;
+my $tpfloor = 1e-5;
+my $tmatfn  = "$hmm_dir/transition_matrices";
+my $meanfn  = "$hmm_dir/means";
+my $varfn   = "$hmm_dir/variances";
+my $minvar  = 1e-4;
+
+# aligned transcripts and the list of aligned files is obtained as a result
+# of (03.) forced alignment
+my ($listoffiles, $transcriptfile);
+if ( $ST::CFG_FORCEDALIGN eq "no" ) {
+    $listoffiles = $ST::CFG_LISTOFFILES;
+    $transcriptfile = $ST::CFG_TRANSCRIPTFILE;
+} else {
+    $listoffiles   = "$ST::CFG_BASE_DIR/falignout/${ST::CFG_EXPTNAME}.alignedfiles";
+    $transcriptfile  = "$ST::CFG_BASE_DIR/falignout/${ST::CFG_EXPTNAME}.alignedtranscripts";
+}
+
+my $topn;
+if ($ST::CFG_HMM_TYPE eq '.cont') {
+    $topn = 1
+} else {
+    $topn = $ST::CFG_FINAL_NUM_DENSITIES;
+}
+my $logdir   = "$ST::CFG_LOG_DIR/$processname";
+my $logfile  = "$logdir/${ST::CFG_EXPTNAME}.$iter-$part.bw.log";
+mkdir ($logdir,0777);
+
+my $ctl_counter = 0;
+open INPUT,"<$listoffiles" or die "Failed to open $listoffiles: $!";
+while (<INPUT>) {
+    $ctl_counter++;
+}
+close INPUT;
+$ctl_counter = int ($ctl_counter / $npart) if $npart;
+$ctl_counter = 1 unless ($ctl_counter);
+
+Log("Baum welch starting for LDA, iteration: $iter ($part of $npart)", 'result');
+
+my $return_value = RunTool
+    ('bw', $logfile, $ctl_counter,
+     -moddeffn => $moddeffn,
+     -ts2cbfn => $statepdeffn,
+     -mixwfn => $mixwfn,
+     -mwfloor => $mwfloor,
+     -tpfloor => $tpfloor,
+     -tmatfn => $tmatfn,
+     -meanfn => $meanfn,
+     -varfn => $varfn,
+     -ltsoov => $ST::CFG_LTSOOV,
+     -dictfn => $ST::CFG_DICTIONARY,
+     -fdictfn => $ST::CFG_FILLERDICT,
+     -ctlfn => $listoffiles,
+     -part => $part,
+     -npart => $npart,
+     -cepdir => $ST::CFG_FEATFILES_DIR,
+     -cepext => $ST::CFG_FEATFILE_EXTENSION,
+     -lsnfn => $transcriptfile,
+     -accumdir => $output_buffer_dir,
+     -varfloor => $minvar,
+     -topn => $topn,
+     -abeam => 1e-90,
+     -bbeam => 1e-10,
+     -agc => $ST::CFG_AGC,
+     -cmn => $ST::CFG_CMN,
+     -varnorm => $ST::CFG_VARNORM,
+     -meanreest => "yes",
+     -varreest => "yes",
+     '-2passvar' => $var2pass,
+     -fullvar => $fullvar,
+     -diagfull => $fullvar,
+     -feat => $ST::CFG_FEATURE,
+     -ceplen => $ST::CFG_VECTOR_LENGTH,
+     -timing => "no");
+
+
+if ($return_value) {
+  LogError("Failed to start bw");
+}
+exit ($return_value);
---
+++ scripts_pl/05.lda_train/slave_lda.pl
@@ -0,0 +1,395 @@
+#!/usr/bin/perl
+## ====================================================================
+##
+## Copyright (c) 1996-2000 Carnegie Mellon University.  All rights
+## reserved.
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions
+## are met:
+##
+## 1. Redistributions of source code must retain the above copyright
+##    notice, this list of conditions and the following disclaimer.
+##
+## 2. Redistributions in binary form must reproduce the above copyright
+##    notice, this list of conditions and the following disclaimer in
+##    the documentation and/or other materials provided with the
+##    distribution.
+##
+## This work was supported in part by funding from the Defense Advanced
+## Research Projects Agency and the National Science Foundation of the
+## United States of America, and the CMU Sphinx Speech Consortium.
+##
+## THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND
+## ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+## THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+## PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
+## NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+##
+## ====================================================================
+##
+## Author: Ricky Houghton
+## Author: David Huggins-Daines
+##
+
+use strict;
+use File::Copy;
+use File::Basename;
+use File::Spec::Functions;
+use File::Path;
+
+use lib catdir(dirname($0), updir(), 'lib');
+use SphinxTrain::Config;
+use SphinxTrain::Util;
+
+if ($ST::CFG_LDA_MLLT ne 'yes') {
+    Log("MODULE: 05 Train LDA transformation\n");
+    Log("Skipped (set \$CFG_LDA_MLLT = 'yes' to enable)\n");
+    exit 0;
+}
+
+my $iter = 1;
+if (@ARGV) {
+    $iter = shift;
+}
+
+my $n_parts = ($ST::CFG_NPART) ? $ST::CFG_NPART : 1;
+# If the number of parts is given as command line argument, overwrite
+# the number coming from the config file
+if (@ARGV) {
+   $n_parts = shift;
+}
+
+$| = 1; # Turn on autoflushing
+my $logdir = "$ST::CFG_LOG_DIR/05.lda_train";
+
+if ($iter == 1) {
+    Log("MODULE: 05 Train LDA transformation\n");
+    Log("Phase 1: Cleaning up directories:");
+    # Don't do this on a queue, because of NFS bugs
+    unless ($ST::CFG_QUEUE_TYPE eq 'Queue::PBS') {
+	LogProgress("\taccumulator...");
+	rmtree ($ST::CFG_BWACCUM_DIR, 0, 1);
+	mkdir ($ST::CFG_BWACCUM_DIR,0777);
+    }
+    LogProgress("logs...");
+    rmtree($logdir, 0, 1);
+    mkdir ($logdir,0777);
+    LogProgress("qmanager...\n");
+    rmtree ($ST::CFG_QMGR_DIR, 0, 1);
+    mkdir ($ST::CFG_QMGR_DIR,0777);
+    LogStatus('completed');
+
+    # If we previously force aligned with single-Gaussian models, use
+    # them for initialization to save some time.
+    if ($ST::CFG_FORCEDALIGN eq 'yes'
+	and $ST::CFG_FALIGN_CI_MGAU eq 'no'
+	and -e catfile($ST::CFG_FORCE_ALIGN_MODELDIR, 'means')) {
+	my $return_value = CopyInitialize();
+	exit ($return_value) if ($return_value);
+    }
+    else {
+	# For the first iteration Flat initialize models.
+	my $return_value = FlatInitialize();
+	exit ($return_value) if ($return_value);
+    }
+    Log("Phase 3: Forward-Backward");
+}
+
+if ($iter eq 'N') {
+    Log("Phase 4: LDA transform estimation");
+    my @deps;
+    for (my $i=1; $i<=$n_parts; $i++) {
+	push @deps, LaunchScript("bw.lda.$iter.$i", ['baum_welch.pl', $iter, $i, $n_parts, 'yes']);
+    }
+    LaunchScript("lda", "lda_train.pl", \@deps);
+}
+else {
+    my @deps;
+    for (my $i=1; $i<=$n_parts; $i++) {
+	push @deps, LaunchScript("bw.lda.$iter.$i", ['baum_welch.pl', $iter, $i, $n_parts, 'no']);
+    }
+    LaunchScript("norm.$iter", ['norm_and_launchbw.pl', $iter, $n_parts], \@deps);
+    # On the first iteration, wait for the LDA stuff to complete
+    my $lda_log = File::Spec->catfile($logdir, "$ST::CFG_EXPTNAME.lda_train.log");
+    if ($iter == 1) {
+	# This is kind of a lousy way to do it, but oh well...
+	local $SIG{CHLD} = sub { wait; };
+	my $interval = 5;
+	while (1) {
+	    # Look for an error
+	    for ($iter = 1; $iter <= $ST::CFG_MAX_ITERATIONS; ++$iter) {
+		my $norm_log = File::Spec->catfile($logdir,
+						   "$ST::CFG_EXPTNAME.$iter.norm.log");
+		if (open LOG, "<$norm_log") {
+		    if (/failed/ or /Aborting/) {
+			LogError("Training failed in iteration $iter");
+			exit 1;
+		    }
+		}
+	    }
+	    if (open LOG, "<$lda_log") {
+		while (<LOG>) {
+		    if (/failed/) {
+			LogError("LDA Training failed");
+			exit 1;
+		    }
+		    elsif (/complete/) {
+			Log("LDA Training completed", 'result');
+			exit 0;
+		    }
+		}
+	    }
+	    sleep $interval;
+	}
+    }
+}
+
+sub CopyInitialize {
+    Log("Phase 2: Copy initialize from falign model\n");
+    my $hmmdir = catdir($ST::CFG_BASE_DIR, "model_parameters");
+    my $outhmm = catdir($hmmdir, "${ST::CFG_EXPTNAME}.ci_${ST::CFG_DIRLABEL}_flatinitial");
+    my $modarchdir = catdir($ST::CFG_BASE_DIR, "model_architecture");
+    mkdir ($outhmm,0777);
+    foreach (qw(means variances mixture_weights transition_matrices)) {
+	copy(catfile($ST::CFG_FORCE_ALIGN_MODELDIR, $_), catfile($outhmm, $_))
+	    or return -1;
+    }
+    my $ci_mdeffile = catfile($modarchdir, "$ST::CFG_EXPTNAME.falign_ci.mdef");
+    my $out_mdeffile = catfile($modarchdir, "$ST::CFG_EXPTNAME.ci.mdef");
+    copy($ci_mdeffile, $out_mdeffile);
+    return 0;
+}
+
+sub FlatInitialize
+{
+    Log("Phase 2: Flat initialize\n");
+
+    #**************************************************************************
+    # this script given an mdef file and a  codebook (means/vars in S3 format)
+    # produces flat mixture weights in a semicontinuos setting. From the models
+    # produced by this script we can restart normal baum-welch training
+    # Flat init might not be the best choice, specially if we have access to
+    # segmentation files for the whole training database.
+    #**************************************************************************
+
+    my $logdir              = "$ST::CFG_LOG_DIR/05.lda_train";
+    my $modarchdir          = "$ST::CFG_BASE_DIR/model_architecture";
+    my $hmmdir              = "$ST::CFG_BASE_DIR/model_parameters";
+    mkdir ($logdir,0777);
+    mkdir ($modarchdir,0777);
+    mkdir ($hmmdir,0777);
+    my $cmd ="";
+
+    #-------------------------------------------------------------------------
+    # Take the phone list. Put three hyphens after every phone. That is the
+    # required format. Then make a ci model definition file from it using the
+    # following program.
+    #-------------------------------------------------------------------------
+
+    #$rawphonefile obtained from variables.def
+    my $phonefile           = "$modarchdir/$ST::CFG_EXPTNAME.phonelist";
+    my $ci_mdeffile         = "$modarchdir/$ST::CFG_EXPTNAME.ci.mdef";
+
+    open PHONELIST, "<".$ST::CFG_RAWPHONEFILE;
+    open PHONEFILE, ">".$phonefile;
+    my $NUM_PHONES = 0;
+    my @phonelist;
+    while (defined(my $line = <PHONELIST>)) {
+      chomp($line);
+      next if $line =~ m/^\s*$/;
+      $line =~ s/$/ - - - /;
+      push @phonelist, $line;
+      $NUM_PHONES++;
+    }
+
+    # Make sure the CI phones are sorted, as PocketSphinx requires them to be
+    foreach (sort @phonelist) {
+	print PHONEFILE $_, "\n";
+    }
+#    system "sed 's+\$+ - - - +g' $ST::CFG_RAWPHONEFILE > $phonefile";
+
+
+    my $logfile = "$logdir/${ST::CFG_EXPTNAME}.make_ci_mdef_fromphonelist.log";
+    #-------------------------------------------------------------------------
+    # Decide on what topology to use for the hmms: 3 state, 5 state, blah state
+    # or what, give it to the variable "statesperhmm" and use it to create
+    # the topology matrix in the topology file
+    #-------------------------------------------------------------------------
+
+    #$statesperhmm obtained from variables.def
+    my $topologyfile             = "$modarchdir/$ST::CFG_EXPTNAME.topology";
+    RunTool('maketopology.pl', $logfile, 0,
+	    $ST::CFG_STATESPERHMM, $ST::CFG_SKIPSTATE, $topologyfile);
+
+    my $return_value;
+    if ($return_value = RunTool('mk_mdef_gen', $logfile, 0,
+				-phnlstfn => $phonefile,
+				-ocimdef => $ci_mdeffile,
+				-n_state_pm => $ST::CFG_STATESPERHMM)) {
+      return $return_value;
+    }
+
+    #-------------------------------------------------------------------------
+    # make the flat models using the above topology file and the mdef file
+    #------------------------------------------------------------------------
+    my $outhmm               = "$hmmdir/${ST::CFG_EXPTNAME}.ci_${ST::CFG_DIRLABEL}_flatinitial";
+    mkdir ($outhmm,0777);
+
+    my $FLAT = "$ST::CFG_BIN_DIR/mk_flat";
+
+    $logfile = "$logdir/${ST::CFG_EXPTNAME}.makeflat_cihmm.log";
+    if ($return_value = RunTool('mk_flat', $logfile, 0,
+				-moddeffn => $ci_mdeffile,
+				-topo => $topologyfile,
+				-mixwfn => catfile($outhmm, 'mixture_weights'),
+				-tmatfn => catfile($outhmm, 'transition_matrices'),
+				-nstream => $ST::CFG_NUM_STREAMS,
+				-ndensity => $ST::CFG_INITIAL_NUM_DENSITIES,
+			       )) {
+      return $return_value;
+    }
+
+    if ($ST::CFG_HMM_TYPE eq ".semi.") {
+      return (0);
+    }
+
+    #-------------------------------------------------------------------------
+    # Accumulate the means from the training data
+    #------------------------------------------------------------------------
+
+    $logfile = "$logdir/${ST::CFG_EXPTNAME}.initmean_cihmm.log";
+
+    open LOG,">$logfile";
+
+    my $output_buffer_dir = "$ST::CFG_BWACCUM_DIR/${ST::CFG_EXPTNAME}_buff_1";
+    mkdir ($output_buffer_dir,0777);
+
+    if ($return_value = RunTool('init_gau', $logfile, 0,
+				-ctlfn => $ST::CFG_LISTOFFILES,
+				-part => 1, -npart => 1,
+				-cepdir => $ST::CFG_FEATFILES_DIR,
+				-cepext => $ST::CFG_FEATFILE_EXTENSION,
+				-accumdir => $output_buffer_dir,
+				-agc => $ST::CFG_AGC,
+				-cmn => $ST::CFG_CMN,
+				-varnorm => $ST::CFG_VARNORM,
+				-feat => $ST::CFG_FEATURE,
+				-ceplen => $ST::CFG_VECTOR_LENGTH,
+			       )) {
+      return $return_value;
+    }
+
+    #-------------------------------------------------------------------------
+    # Normalize the means
+    #------------------------------------------------------------------------
+
+    $logfile = "$logdir/${ST::CFG_EXPTNAME}.normmean_cihmm.log";
+
+    if ($return_value = RunTool('norm', $logfile, 0,
+				-accumdir => $output_buffer_dir,
+				-meanfn => catfile($outhmm, "globalmean"),
+			       )) {
+      return $return_value;
+    }
+
+    #-------------------------------------------------------------------------
+    # Accumulate the variances from the training data
+    #------------------------------------------------------------------------
+
+    $logfile = "$logdir/${ST::CFG_EXPTNAME}.initvar_cihmm.log";
+
+    if ($return_value = RunTool('init_gau', $logfile, 0,
+				-meanfn => catfile($outhmm, "globalmean"),
+				-ctlfn => $ST::CFG_LISTOFFILES,
+				-part => 1,  -npart => 1,
+				-cepdir => $ST::CFG_FEATFILES_DIR,
+				-cepext => $ST::CFG_FEATFILE_EXTENSION,
+				-accumdir => $output_buffer_dir,
+				-agc => $ST::CFG_AGC,
+				-cmn => $ST::CFG_CMN,
+				-varnorm => $ST::CFG_VARNORM,
+				-feat => $ST::CFG_FEATURE,
+				-ceplen => $ST::CFG_VECTOR_LENGTH,
+				-fullvar => $ST::CFG_FULLVAR,
+			       )) {
+      return $return_value;
+    }
+
+    #-------------------------------------------------------------------------
+    # Normalize the variances
+    #------------------------------------------------------------------------
+
+    $logfile = "$logdir/${ST::CFG_EXPTNAME}.normvar_cihmm.log";
+
+    if ($return_value = RunTool('norm', $logfile, 0,
+				-accumdir => $output_buffer_dir,
+				-fullvar => $ST::CFG_FULLVAR,
+				-varfn => catfile($outhmm, "globalvar"),
+			       )) {
+      return $return_value;
+    }
+
+
+    #-------------------------------------------------------------------------
+    # Create the copy operation file, simply a map between states
+    #------------------------------------------------------------------------
+
+    my $NUM_CI_STATES = $NUM_PHONES * $ST::CFG_STATESPERHMM;
+    if (open CPFILE, ">$ST::CFG_CP_OPERATION") {
+      for (my $CI_STATE = 0; $CI_STATE < $NUM_CI_STATES; $CI_STATE++) {
+	print CPFILE "$CI_STATE\t0\n";
+      }
+      close(CPFILE);
+    } else {
+      warn "Can't open $ST::CFG_CP_OPERATION\n";
+    }
+
+    #-------------------------------------------------------------------------
+    # Copy the means to all other states
+    #------------------------------------------------------------------------
+
+    $logfile = "$logdir/${ST::CFG_EXPTNAME}.cpmean_cihmm.log";
+
+    if ($return_value = RunTool('cp_parm', $logfile, 0,
+				-cpopsfn => $ST::CFG_CP_OPERATION,
+				-igaufn => catfile($outhmm, 'globalmean'),
+				-ncbout => $NUM_CI_STATES,
+				-ogaufn => catfile($outhmm, 'means')
+			       )) {
+      return $return_value;
+    }
+
+    #-------------------------------------------------------------------------
+    # Copy the variances to all other states
+    #------------------------------------------------------------------------
+
+    $logfile = "$logdir/${ST::CFG_EXPTNAME}.cpvar_cihmm.log";
+
+    my @varcpy;
+    if ($ST::CFG_FULLVAR eq 'yes') {
+	@varcpy = (-ifullgaufn => catfile($outhmm, 'globalvar'),
+		   -ofullgaufn => catfile($outhmm, 'variances'));
+    }
+    else {
+	@varcpy = (-igaufn => catfile($outhmm, 'globalvar'),
+		   -ogaufn => catfile($outhmm, 'variances'));
+    }
+    if ($return_value = RunTool('cp_parm', $logfile, 0,
+				-cpopsfn => $ST::CFG_CP_OPERATION,
+				-ncbout => $NUM_CI_STATES,
+				@varcpy
+			       )) {
+	return $return_value;
+    }
+
+    unlink $ST::CFG_CP_OPERATION;
+
+    return $return_value;
+}
