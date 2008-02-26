cd8693dca5b8210bda642b35175a9f534976a4fc
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-02-26 22:07:26 +0000

    right now this is a script to "undump" pocketsphinx sendump files, in the future it will be a module to access them
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@7873 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ python/sphinx/sendump.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.insert(0, '/home/dhuggins/Projects/Sphinx/SphinxTrain/python')
+import sphinx.s3mixw
+import struct
+import numpy
+
+def usage():
+    print "Usage: %s IN_SENDUMP OUT_MIXW" % sys.argv[0]
+
+def readstr(fh):
+    nbytes = struct.unpack('I', fh.read(4))[0]
+    if nbytes == 0:
+        return None
+    else:
+        return fh.read(nbytes)
+
+if len(sys.argv) < 3:
+    usage()
+    sys.exit(2)
+
+sendump = open(sys.argv[1])
+title = readstr(sendump)
+while True:
+    header = readstr(sendump)
+    if header == None:
+        break
+
+# Number of codewords and pdfs
+r, c = struct.unpack('II', sendump.read(8))
+print "rows: %d, columns: %d" % (r,c)
+
+# Now read the stuff
+opdf_8b = numpy.empty((c,4,r))
+for i in range(0,4):
+    for j in range(0,r):
+        # Read bytes, expand to ints, shift them up
+        mixw = numpy.fromfile(sendump, 'B', c).astype('i') << 10
+        # Negate, exponentiate, and untranspose
+        opdf_8b[:,i,j] = numpy.power(1.0001, -mixw)
+        
+sphinx.s3mixw.open(sys.argv[2], 'wb').writeall(opdf_8b)
