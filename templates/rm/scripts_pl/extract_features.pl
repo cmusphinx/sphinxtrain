e06b243e94d22455dd89dedcb6e4fbdbe85e9a89
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-07-10 18:30:39 +0000

    Add an --inctl option which allows you to limit the files extracted to a specific output set (this sounds backwards, but we don't have actual control files for the input)
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@8605 94700074-3cef-4d97-a70e-9c8c206c02f5

--- templates/rm/scripts_pl/extract_features.pl
+++ templates/rm/scripts_pl/extract_features.pl
@@ -19,11 +19,23 @@ GetOptions(\%opts,
 	   'params|p=s',
 	   'outdir|o=s',
 	   'outext|e=s',
+	   'inctl|c=s',
 	   'help|h|?')
     or pod2usage(2);
 pod2usage(0) if $opts{help};
 
 my %dirs = parse_config($opts{config});
+
+my %inctl;
+if (defined($opts{inctl}) and open INCTL, "<$opts{inctl}") {
+    while (<INCTL>) {
+	chomp;
+	my @fields = split;
+	my $fileid = shift @fields;
+	$fileid =~ tr,\\,/,;
+	$inctl{$fileid}++;
+    }
+}
 
 my @feat_params;
 if (open PARAMS, "<$opts{params}") {
@@ -34,8 +46,12 @@ if (open PARAMS, "<$opts{params}") {
 	push @feat_params, split;
     }
 }
+if (@ARGV) {
+    push @feat_params, @ARGV;
+}
 
-foreach my $dir (qw(dep ind dep_trn ind_trn)) {
+my @parts = qw(dep ind dep_trn ind_trn);
+foreach my $dir (@parts) {
     my $indir = $dirs{$dir};
     my $outdir = rel2abs(catdir($opts{outdir}, $dir));
     mkdir($opts{outdir}, 0777);
@@ -60,7 +76,9 @@ foreach my $dir (qw(dep ind dep_trn ind_trn)) {
 	 }, $indir);
 
     my ($fh, $filename) = tempfile();
-    foreach (@lcfiles) { print $fh "$_\n" }
+    foreach (@lcfiles) {
+	print $fh "$_\n" unless %inctl and not exists $inctl{"$dir/$_"};
+    }
     close($fh);
     my $rv = system('sphinx_fe',
 		    -di => $indir, -do => $outdir,
@@ -70,6 +88,7 @@ foreach my $dir (qw(dep ind dep_trn ind_trn)) {
     unlink($filename);
     die "sphinx_fe failed: $rv" unless $rv == 0;
     foreach (@ucfiles) {
+	next if %inctl and not exists $inctl{"$dir/" . lc($_)};
 	my $infile = catfile($indir, $_) . '.WAV';
 	my $outfile = catfile($outdir, lc($_)) . '.' . $opts{outext};
 	$rv = system('sphinx_fe',
@@ -89,10 +108,12 @@ extract_features.pl - Create acoustic feature files for RM1
 
 =head1 SYNOPSIS
 
-create_features.pl
-     S<[<I<--config> B<etc/rm1_files.cfg>]>
+extract_features.pl
+     S<[ I<--config> B<etc/rm1_files.cfg> ]>
      S<[ I<--params> B<etc/feat.params> ]>
      S<[ I<-o> B</path/to/training/feat> ]>
+     S<[ I<-e> I<EXTENSION> ]>
+     S<[ I<-c> I<CONTROL-FILE> ]>
 
 =head1 DESCRIPTION
 
