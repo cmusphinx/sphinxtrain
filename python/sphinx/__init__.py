4bbed7ead145b22af19de8fffc958556397611d6
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-09-23 03:56:32 +0000

    Add htkmfc.py
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@8779 94700074-3cef-4d97-a70e-9c8c206c02f5

--- python/sphinx/__init__.py
+++ python/sphinx/__init__.py
@@ -5,7 +5,7 @@ SphinxTrain Python Modules.
 __all__ = ['arpalm', 'mfcc', 'mllt', 's2mfc', 's3file',
            's3gaucnt', 's3gau', 's3lda', 's3mdef',
            's3mixw', 's3model', 's3tmat', 'feat', 'gmm',
-           'divergence']
+           'divergence', 'hypseg']
 
 __author__ = "David Huggins-Daines <dhuggins@cs.cmu.edu>"
 __version__ = "$Revision$"
