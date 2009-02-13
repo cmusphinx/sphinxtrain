19ba47ddef57dbc951235c0810183626ab60e174
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2009-02-13 21:38:54 +0000

    Still actually untested implementation of MLLR for variances
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@8989 94700074-3cef-4d97-a70e-9c8c206c02f5

--- python/sphinx/mllr.py
+++ python/sphinx/mllr.py
@@ -29,7 +29,8 @@ import s3lda
 
 def estimate_mllr_mean(stats, inmean, invar):
     """
-    Estimate an MLLR transformation based on observed statistics.
+    Estimate an MLLR transformation of the means based on observed
+    statistics.
     
     This function calculates an MLLR transformation W (an n by n+1
     matrix) which, when applied to C{inmean}, maximizes the likelihood
@@ -38,8 +39,9 @@ def estimate_mllr_mean(stats, inmean, invar):
     Currently this does only one class and one stream, but it will
     promptly be extended once the "learning exercise" is over.
 
-    @param stats: Diagonal covariance observation counts, as returned
+    @param stats: Observation counts, as returned
                   by C{sphinx.s3gaucnt.accumdirs}
+                  or C{sphinx.s3gaucnt.accumdirs_full}.
     @type stats: sphinx.s3gaucnt.S3GauCnt
     @param inmean: Input mean parameters
     @type inmean: sphinx.s3gau.S3Gau
@@ -59,17 +61,24 @@ def estimate_mllr_mean(stats, inmean, invar):
     # Actual mean and variance parameters
     m = inmean.getall()
     v = invar.getall()
+    # Observation counts
+    vc = stats.getvars()
+    mc = stats.getmeans()
+    dc = stats.getdnom()
     # One-class MLLR: just sum over all densities
     for i in range(0, means.n_mgau):
         for k in range(0, means.density):
             # Extended mean vector
-            xmean = np.concatenate(1, m[i,0,k])
-            # Inverse variance
-            invvar = 1./v[i,0,k].clip(0,1e-5)
+            xmean = np.concatenate(1, m[i][0][k])
+            # Inverse variance (also use only the diagonal)
+            invvar = v[i][0][k]
+            if len(invvar.shape) > 1:
+                invvar = np.diag(invvar)
+            invvar = 1./invvar.clip(0,1e-5)
             # Sum of posteriors (i.e. sum_t L_m_r(t))
-            dnom = stats.getdnom()[i,0,k]
+            dnom = dc[i,0,k]
             # Sum of mean statistics
-            obsmean = stats.getmeans()[i,0,k]
+            obsmean = mc[i][0][k]
             
             for l in range(0, ndim):
                 G[l] += dnom * invvar[l] * np.outer(xmean, xmean)
@@ -78,6 +87,79 @@ def estimate_mllr_mean(stats, inmean, invar):
     for i in range(0, ndim):
         W[i] = np.linalg.solve(G[i], Z[i])
     return W
+
+def estimate_mllr_variance(stats, inmean, invar, W):
+    """
+    Estimate an MLLR transformation of the variances based on observed
+    statistics.
+    
+    This function calculates an MLLR transformation H (an n by n
+    matrix) which maximizes the likelihood of the data as represented
+    by C{stats}, when applied to the inverse Cholesky factor of the
+    covariance matrix B as B^T H B.  For diagonal covariances this
+    reduces to a scaling of the variance by the diagonals of H, since
+    the diagonal b = (sqrt(var^{-1}))^{-1} = var^{0.5}.
+
+    Note that this function will raise an exception if -2passvar yes
+    was enabled when collecting the observation counts, since it
+    requires them to consist of the sum of the outer products of the
+    observation vectors scaled by their posterior probabilities,
+    (L_m_r(t)o(t)o(t)^T in Cambridge papers).
+
+    Currently this does only one class and one stream, but it will
+    promptly be extended once the "learning exercise" is over.
+
+    @param stats: Observation counts, as returned
+                  by C{sphinx.s3gaucnt.accumdirs}
+                  or C{sphinx.s3gaucnt.accumdirs_full}.
+    @type stats: sphinx.s3gaucnt.S3GauCnt
+    @param inmean: Input mean parameters
+    @type inmean: sphinx.s3gau.S3Gau
+    @param invar: Input covariance parameters
+    @type inmvar: sphinx.s3gau.S3Gau
+    @param W: Previously computed MLLR transformation of means
+    @ptype W: numpy.ndarray
+    @return: MLLR transformation of variances
+    @rtype: numpy.ndarray
+    """
+    if stats.pass2var:
+        raise RuntimeException, "Statistics using -2passvar yes are not allowed"
+    ndim = inmean.veclen[0]
+    # Output matrix H
+    H = np.zeros((ndim, ndim))
+    # Actual mean and variance parameters
+    m = inmean.getall()
+    v = invar.getall()
+    # Observation counts
+    vc = stats.getvars()
+    mc = stats.getmeans()
+    dc = stats.getdnom()
+    # One-class MLLR: just sum over all densities
+    norm = 0
+    for i in range(0, means.n_mgau):
+        for k in range(0, means.density):
+            # Extended mean vector
+            xmean = np.concatenate(1, m[i][0][k])
+            # Transform it
+            mean = np.dot(W, xmean)
+            # Calcluate C (Cholesky factor of inverse variance)
+            invvar = v[i][0][k].clip(0,1e-5)
+            if len(invvar.shape) == 1:
+                invvar = np.diag(invvar)
+            C = np.linalg.cholesky(invvar)
+            # sum(L_m_r o o^T) (obs squared)
+            nom = vc[i][0][k]
+            # \hat mu_m_r \bar o_m_r^T (cross term 1)
+            nom -= np.outer(mean, mc[i][0][k])
+            # \bar o_m_r^T \hat mu_m_r^T (cross term 2)
+            nom -= np.outer(mc[i][0][k], mean)
+            # \mu_m_r \mu_m_r^T sum(L_m_r) (mean squared)
+            nom += np.outer(mean, mean) * dc[i][0][k]
+            # Multiply in Cholesky factors and accumulate
+            H += np.dot(np.dot(C, nom), C.T)
+            # Accumulate normalizer
+            norm += dc[i][0][k]
+    return H / norm
 
 if __name__ == '__main__':
     def usage():
