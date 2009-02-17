53542c1b7150e505ddfa5fb9565eb1ce44013cb5
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2009-02-17 17:52:36 +0000

    Add MLLR module
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@8997 94700074-3cef-4d97-a70e-9c8c206c02f5

--- python/sphinx/__init__.py
+++ python/sphinx/__init__.py
@@ -3,7 +3,7 @@ SphinxTrain Python Modules.
 """
 
 __all__ = ['arpalm', 'mfcc', 'mllt', 's2mfc', 's3file',
-           's3gaucnt', 's3gau', 's3lda', 's3mdef',
+           's3gaucnt', 's3gau', 's3lda', 's3mdef', 'mllr',
            's3mixw', 's3model', 's3tmat', 'feat', 'gmm',
            'divergence', 'hypseg']
 
