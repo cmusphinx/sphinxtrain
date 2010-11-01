a29cda9232c5e5babbbd94a368a4b256b30642ef
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2010-11-01 16:48:04 +0000

    add a pruning option to lattice error calculation (but you should use fstutils)
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@10586 94700074-3cef-4d97-a70e-9c8c206c02f5

--- python/cmusphinx/lattice_error.py
+++ python/cmusphinx/lattice_error.py
@@ -5,7 +5,10 @@ import os
 import lattice
 from itertools import izip
 
-ctl, ref, latdir = sys.argv[1:]
+ctl, ref, latdir = sys.argv[1:4]
+prune = 0
+if len(sys.argv) > 4:
+    prune = float(sys.argv[4])
 
 ctl = open(ctl)
 ref = open(ref)
@@ -17,6 +20,7 @@ for c,r in izip(ctl, ref):
     del r[-1]
     if len(r) == 0 or r[0] != '<s>': r.insert(0, '<s>')
     if r[-1] != '</s>': r.append('</s>')
+    nw = len(r) - 2
     r = filter(lambda x: not lattice.is_filler(x), r)
     l = lattice.Dag()
     try:
@@ -26,13 +30,18 @@ for c,r in izip(ctl, ref):
             l.sphinx2dag(os.path.join(latdir, c + ".lat"))
         except IOError:
             l.htk2dag(os.path.join(latdir, c + ".slf"))
+    if prune:
+        l.posterior_prune(-prune)
     err, bt = l.minimum_error(r)
     maxlen = [max([len(y) for y in x]) for x in bt]
     print " ".join(["%*s" % (m, x[0]) for m, x in izip(maxlen, bt)])
     print " ".join(["%*s" % (m, x[1]) for m, x in izip(maxlen, bt)])
-    print "Error: %.2f%%" % (float(err) / len(r) * 100)
+    if nw:
+        print "Error: %.2f%%" % (float(err) / nw * 100)
+    else:
+        print "Error: %.2f%%" % 0
     print
-    wordcount += len(r)
+    wordcount += nw
     errcount += err
 
 print "TOTAL Error: %.2f%%" % (float(errcount) / wordcount * 100)
