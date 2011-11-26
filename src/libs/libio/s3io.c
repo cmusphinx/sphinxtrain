8421e6357f82793d0555945f7f82d2ccf6b4f1be
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2011-11-26 14:05:54 +0000

    Fixes windows build
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@11290 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/libs/libio/s3io.c
+++ src/libs/libio/s3io.c
@@ -46,8 +46,10 @@
 
 #include <s3/s3io.h>
 #include <s3/swap.h>
-#include <sphinxbase/ckd_alloc.h>
 #include <s3/s3.h>
+#include <sys_compat/file.h>
+
+#include <sphinxbase/ckd_alloc.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -807,3 +809,427 @@ s3write_1d(void *arr,
 
     return S3_SUCCESS;
 }
+
+
+int
+areadchar (char *file,
+	   char **data_ref,
+	   int *length_ref)
+{
+  int             fd;
+  int             length;
+  char           *data;
+
+  if ((fd = open (file, O_RDONLY | O_BINARY, 0644)) < 0)
+  {
+    fprintf (stderr, "areadchar: %s: can't open\n", file);
+    return -1;
+  }
+  if (read (fd, (char *) &length, 4) != 4)
+  {
+    fprintf (stderr, "areadchar: %s: can't read length (empty file?)\n", file);
+    close (fd);
+    return -1;
+  }
+  SWAP_INT32(&length);
+  /* Just get the file size if we were not given a buffer. */
+  if (data_ref == NULL) {
+	  close(fd);
+	  if (length_ref)
+	    *length_ref = length;
+	  return length;
+  }
+  if (!(data = malloc ((unsigned) length)))
+  {
+    fprintf (stderr, "areadchar: %s: can't alloc data\n", file);
+    close (fd);
+    return -1;
+  }
+  if (read (fd, data, length) != length)
+  {
+    fprintf (stderr, "areadchar: %s: can't read data\n", file);
+    close (fd);
+    free (data);
+    return -1;
+  }
+  close (fd);
+  *data_ref = data;
+  *length_ref = length;
+  return length;
+}
+
+/* Macro to byteswap an int variable.  x = ptr to variable */
+#define MYSWAP_INT(x)   *(x) = ((0x000000ff & (*(x))>>24) | \
+                                (0x0000ff00 & (*(x))>>8) | \
+                                (0x00ff0000 & (*(x))<<8) | \
+                                (0xff000000 & (*(x))<<24))
+/* Macro to byteswap a float variable.  x = ptr to variable */
+#define MYSWAP_FLOAT(x) MYSWAP_INT((int *) x)
+
+int get_length(char *file,
+               int *byterev)
+{
+  FILE            *fp;
+  int             length;
+  int             n;
+  struct stat statbuf;
+
+  *byterev = 0;
+  if (stat(file, &statbuf) < 0) {
+      printf("stat_retry(%s) failed\n", file);
+      return -1;
+  }
+  if ((fp = fopen(file, "rb")) == NULL) {
+      printf("fopen(%s,rb) failed\n", file);
+      return -1;
+  }
+
+  /* Read #floats in header */
+  if (fread(&length, sizeof(int), 1, fp) != 1) {
+      fclose (fp);
+      return -1;
+  }
+
+  /* Check if length matches file size */
+  if ((length*sizeof(float) + 4) != statbuf.st_size) {
+      n = length;
+      MYSWAP_INT(&n);
+
+      if ((n*sizeof(float) + 4) != statbuf.st_size) {
+          printf("Header size field: %d(%08x); filesize: %d(%08x)\n",
+                  length, length, (int)statbuf.st_size, (int)statbuf.st_size);
+          fflush(stdout);
+          fclose (fp);
+          return -1;
+      }
+
+      length = n;
+      *byterev = 1;
+  }
+  if (length <= 0) {
+      printf("Header size field: %d\n",  length); fflush(stdout);
+      fclose (fp);
+      return -1;
+  }
+  fclose(fp);
+  return length;
+}
+
+int
+areadfloat (char *file,
+            float **data_ref,
+            int *length_ref)
+{
+  FILE            *fp;
+  int             length;
+  int             ret, byterev;
+  int             offset;
+  float           *data;
+  int dummy_length;
+
+  /* Get the file length and whether the file needs to be byte-reversed */
+  if ((length = get_length(file, &byterev)) < 0) {
+      return -1;
+  }
+  if ((fp = fopen(file, "rb")) == NULL) {
+      printf("fopen(%s,rb) failed\n", file);
+      return -1;
+  }
+
+  /* Read #floats in header, so we start reading at the right position */
+  if (fread(&dummy_length, sizeof(int), 1, fp) != 1) {
+      fclose (fp);
+      return -1;
+  }
+
+  /* Just get the file size if we were not given a buffer. */
+  if (data_ref == NULL) {
+          fclose(fp);
+          if (length_ref)
+            *length_ref = length;
+          return length;
+  }
+
+  if ((data = (float *) calloc (length,sizeof(float))) == NULL)
+  {
+    fprintf (stderr, "areadfloat: %s: can't alloc data\n", file); 
+    fflush(stdout);
+    fclose (fp);
+    return -1;
+  }
+  if ((ret = fread(data,sizeof(float),length,fp)) != length)
+  {
+    fprintf (stderr,"areadfloat: %s: expected %d, got %d\n",file,length,ret);
+    fflush(stdout);
+    fclose (fp);
+    free (data);
+    return -1;
+  }
+  fclose (fp);
+  *data_ref = (float *) data;
+  if (byterev==1) 
+     for(offset = 0; offset < length; offset++) MYSWAP_FLOAT(*data_ref+offset);
+  *length_ref = length;
+  return length;
+}
+
+int
+areadfloat_part (char *file,
+                 int s_coeff,
+                 int e_coeff,
+                 float **data_ref,
+                 int *length_ref)
+{
+    static char p_file[MAXPATHLEN] = "";
+    static FILE *fp = NULL;
+    static int len;
+    static int byterev;
+    int r_len;
+    float *r_buf;
+    int i;
+    int dummy_length;
+
+    assert(s_coeff <= e_coeff);
+    if (strcmp(file, p_file) != 0) {
+        if (fp) {
+            fclose(fp);
+        }
+        /* Get the file length and whether the file needs to be
+           byte-reversed */
+        if ((len = get_length(file, &byterev)) < 0) {
+            return -1;
+        }
+        fp = fopen(file, "rb");
+        if (fp == NULL) {
+            fprintf(stderr, "areadfloat_part: unable to open %s for reading;", file);
+            perror("");
+            *data_ref = NULL;
+            *length_ref = 0;
+
+            return -1;
+        }
+        strcpy(p_file, file);
+
+        if (fread(&dummy_length, sizeof(int), 1, fp) != 1) {
+            fprintf(stderr, "areadfloat_part: unable to read length from %s;", file);
+            perror("");
+
+            *data_ref = NULL;
+            *length_ref = 0;
+
+            return -1;
+        }
+    }
+
+    if (s_coeff >= len) {
+        fprintf(stderr, "areadfloat_part: start of data beyond end of file\n");
+        *data_ref = NULL;
+        *length_ref = 0;
+
+        return 0;
+    }
+
+    if (e_coeff >= len) {
+        fprintf(stderr, "areadfloat_part: end of data beyond end of file; resetting\n");
+
+        e_coeff = len-1;
+    }
+
+    if (fseek(fp, s_coeff * sizeof(float) + sizeof(int), SEEK_SET) < 0) {
+        fprintf(stderr, "areadfloat_part: seek fail;");
+        perror("");
+        fprintf(stderr, "offset == %zu in %s\n",
+                s_coeff * sizeof(float) + sizeof(int), file);
+
+        *data_ref = NULL;
+        *length_ref = 0;
+        return -1;
+    }
+
+    r_len = e_coeff - s_coeff + 1;
+
+    assert(s_coeff + r_len <= len);
+    /* Just get the file size if we were not given a buffer. */
+    if (data_ref == NULL) {
+        if (length_ref)
+            *length_ref = r_len;
+        return r_len;
+    }
+
+    r_buf = calloc(r_len, sizeof(float));
+    if (fread(r_buf, sizeof(float), r_len, fp) != r_len) {
+        fprintf(stderr, "areadfloat_part: unable to read %d coeff @ %d from %s\n",
+                r_len, s_coeff, file);
+        free(r_buf);
+        *data_ref = NULL;
+        *length_ref = 0;
+        return -1;
+    }
+
+  if (byterev==1) 
+    for (i = 0; i < r_len; i++) {
+        MYSWAP_FLOAT(&r_buf[i]);
+    }
+
+    *data_ref = r_buf;
+    *length_ref = r_len;
+
+    return r_len;
+}
+
+
+
+
+int
+areadint (char *file,
+	  int **data_ref,
+	  int *length_ref)
+{
+  int             fd;
+  int             length;
+  int             size;
+  int             offset;
+  char           *data;
+
+  if ((fd = open (file, O_RDONLY | O_BINARY, 0644)) < 0)
+  {
+    fprintf (stderr, "areadint: %s: can't open\n", file);
+    return -1;
+  }
+  if (read (fd, (char *) &length, 4) != 4)
+  {
+    fprintf (stderr, "areadint: %s: can't read length (empty file?)\n", file);
+    close (fd);
+    return -1;
+  }
+  SWAP_INT32(&length);
+  /* Just get the file size if we were not given a buffer. */
+  if (data_ref == NULL) {
+	  close(fd);
+	  if (length_ref)
+	    *length_ref = length;
+	  return length;
+  }
+  size = length * sizeof (int);
+  if (!(data = malloc ((unsigned) size)))
+  {
+    fprintf (stderr, "areadint: %s: can't alloc data\n", file);
+    close (fd);
+    return -1;
+  }
+  if (read (fd, data, size) != size)
+  {
+    fprintf (stderr, "areadint: %s: can't read data\n", file);
+    close (fd);
+    free (data);
+    return -1;
+  }
+  close (fd);
+  *data_ref = (int *) data;
+  for(offset = 0; offset < length; offset++)
+    SWAP_INT32(*data_ref + offset);
+  *length_ref = length;
+  return length;
+}
+
+int
+areadshort (char *file,
+	    short **data_ref,
+	    int *length_ref)
+{
+  int             fd;
+  int             length;
+  int             size;
+  int             offset;
+  char           *data;
+
+  if ((fd = open (file, O_RDONLY | O_BINARY, 0644)) < 0)
+  {
+    fprintf (stderr, "areadshort: %s: can't open\n", file);
+    perror("");
+    return -1;
+  }
+  if (read (fd, (char *) &length, 4) != 4)
+  {
+    fprintf (stderr, "areadshort: %s: can't read length (empty file?)\n", file);
+    perror("");
+    close (fd);
+    return -1;
+  }
+  SWAP_INT32(&length);
+  /* Just get the file size if we were not given a buffer. */
+  if (data_ref == NULL) {
+	  close(fd);
+	  if (length_ref)
+	    *length_ref = length;
+	  return length;
+  }
+
+  size = length * sizeof (short);
+  if (!(data = malloc ((unsigned) size)))
+  {
+    fprintf (stderr, "areadshort: %s: can't alloc data\n", file);
+    perror("");
+    close (fd);
+    return -1;
+  }
+  if (read (fd, data, size) != size)
+  {
+    fprintf (stderr, "areadshort: %s: can't read data\n", file);
+    perror("");
+    close (fd);
+    free (data);
+    return -1;
+  }
+  close (fd);
+  *data_ref = (short *) data;
+  for(offset = 0; offset < length; offset++)
+    SWAP_INT16(*data_ref + offset);
+  *length_ref = length;
+  return length;
+}
+
+int
+awritefloat (char *file,
+	     float *data,
+	     int length)
+{
+  int             fd;
+  int             size;
+  int             offset;
+
+  if ((fd = open (file, O_CREAT | O_WRONLY | O_TRUNC | O_BINARY, 0644)) < 0)
+  {
+    fprintf (stderr, "awritefloat: %s: can't create\n", file);
+    perror("");
+    return -1;
+  }
+
+  SWAP_INT32(&length);
+  if (write (fd, (char *) &length, 4) != 4)
+  {
+    fprintf (stderr, "awritefloat: %s: can't write length\n", file);
+    perror("");
+    close (fd);
+    return -1;
+  }
+  SWAP_INT32(&length);
+
+  for(offset = 0; offset < length; offset++)
+    SWAP_FLOAT32(data + offset);
+  size = length * sizeof (float);
+  if (write (fd, (char *) data, size) != size)
+  {
+    fprintf (stderr, "awritefloat: %s: can't write data\n", file);
+    perror("");
+    close (fd);
+    return (-1);
+  }
+  for(offset = 0; offset < length; offset++)
+    SWAP_FLOAT32(data + offset);
+
+  printf ("Wrote %d floats in %s.\n", length, file);
+  close (fd);
+  return length;
+}
+
