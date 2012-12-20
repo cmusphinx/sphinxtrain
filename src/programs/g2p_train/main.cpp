1e800663983ca431491f0b98179695da0c7f7d33
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-12-20 22:36:47 +0000

    Post-release fixes:
    Seek in lib64 too, to be compatible with some 64-bit distributions
    
    Fix distcheck when g2p is enabled
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/SphinxTrain@11695 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/programs/g2p_train/main.cpp
+++ src/programs/g2p_train/main.cpp
@@ -1,3 +1,39 @@
+/* ====================================================================
+ * Copyright (c) 1995-2012 Carnegie Mellon University.  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * This work was supported in part by funding from the Defense Advanced 
+ * Research Projects Agency and the National Science Foundation of the 
+ * United States of America, and the CMU Sphinx Speech Consortium.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
+ * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ *
+ */
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -8,7 +44,7 @@
 using namespace std;
 
 const char helpstr[] =
-		"Usage: g2p_train [-seq1_del] [-seq2_del] [-seq1_max SEQ1_MAX] [-seq2_max SEQ2_MAX] \n\ 
+		"Usage: g2p_train [-seq1_del] [-seq2_del] [-seq1_max SEQ1_MAX] [-seq2_max SEQ2_MAX] \n\
 	               [-seq_sep SEQ_SEP] [-s1s2_sep S1S2_SEP] [-eps EPS]   \n\
 	               [-skip SKIP] [-seq1in_sep SEQ1IN_SEP] [-seq2in_sep SEQ2IN_SEP]  \n\
 	               [-s1s2_delim S1S2_DELIM] [-iter ITER] [-order ORDER] [-smooth SMOOTH]  \n\
