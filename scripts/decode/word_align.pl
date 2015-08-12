6fcd71ec7091c1f06ea76c9d4171b8b939e9811d
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2015-08-12 00:22:55 +0000

    Wrapped long lines, enhanced die messages, fixed deprecated encoding pragma (and corrupted output) with recent Perls, updated POD, introduced two new options for less strict processing
    Patch by Laimonas Vebra
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinxtrain@13103 94700074-3cef-4d97-a70e-9c8c206c02f5

--- scripts/decode/word_align.pl
+++ scripts/decode/word_align.pl
@@ -8,23 +8,44 @@
 # October 2004.
 
 use strict;
-use Getopt::Long;
 use Pod::Usage;
-use vars qw($Verbose $CER $IgnoreUttID);
-use encoding 'utf8';
+use Getopt::Long;
 
-my ($help,%hyphash);
+use open ':encoding(utf8)';
+binmode(STDOUT, ":utf8");
+
+use constant INS => 1;
+use constant DEL => 2;
+use constant MATCH => 3;
+use constant SUBST => 4;
+use constant BIG_NUMBER => 1e50;
+
+use vars qw($Help $Verbose $CER $IgnoreUttID $SkipComments $RefHypMatch);
+
+# Defaults
+$IgnoreUttID = 0;
+$SkipComments = 0;
+$RefHypMatch = 1;
+
 GetOptions(
-	   'help|?' => \$help,
-	   'verbose|v' => \$Verbose,
-	   'cer|c' => \$CER,
-	   'ignore-uttid|i' => \$IgnoreUttID,
-	  ) or pod2usage(1);
-pod2usage(1) if $help;
+    'help|?' => \$Help,
+    'verbose|v' => \$Verbose,
+    'cer|c' => \$CER,
+    'ignore-uttid|i' => \$IgnoreUttID,
+    'comments!' => \$SkipComments,
+    'ref-hyp-match!' => \$RefHypMatch
 
+    ) or pod2usage(1);
+
+pod2usage(1) if $Help;
 pod2usage(2) unless @ARGV == 2;
-my ($ref, $hyp) = @ARGV;
 
+my %hyphash;
+my ($ref, $hyp) = @ARGV;
+my ($total_cost, $total_words, $total_hyp);
+my ($total_ins, $total_del, $total_subst, $total_match);
+
+# Build hypotesis hash (lookup by uttid)
 open HYP, "<$hyp" or die "Failed to open $hyp: $!";
 while (defined(my $hyp_utt=<HYP>)){
     my $hyp_uttid;
@@ -36,37 +57,44 @@ close HYP;
 open REF, "<$ref" or die "Failed to open $ref: $!";
 open HYP, "<$hyp" or die "Failed to open $hyp: $!";
 
-use constant INS => 1;
-use constant DEL => 2;
-use constant MATCH => 3;
-use constant SUBST => 4;
-use constant BIG_NUMBER => 1e50;
-
-my ($total_words, $total_match, $total_cost, $total_hyp);
-my ($total_ins, $total_del, $total_subst);
+# Main loop
 while (defined(my $ref_utt = <REF>)) {
-    my $hyp_utt;
-    my $ref_uttid;
-    my $hyp_uttid;
+    my (@res, $ref_uttid, $hyp_utt, $hyp_uttid);
 
     last unless defined $ref_utt;
 
-    ($ref_utt,$ref_uttid)=s3_magic_norm($ref_utt);
+    # Normalize reference utterance
+    @res = s3_magic_norm($ref_utt);
 
-    if(defined $IgnoreUttID){
+    # Skip empty and commented lines; s3_magic_norm() returns undef.
+    next unless grep defined, @res;
+
+    ($ref_utt, $ref_uttid) = @res;
+
+    if ($IgnoreUttID) {
 	$hyp_utt = <HYP>;
-	die "UttID is ignored but file size mismatch between $ref and $hyp" unless defined($hyp_utt);
-    }else{
-	$hyp_utt=$hyphash{$ref_uttid};
-        die "UttID is not ignored but it could not found in any entries of the hypothesis file on line3 $. UTTID: $ref_uttid\n" unless defined($hyp_utt);
+	die "UTTID is ignored when matching reference and hypothesis ",
+	"files and line count of files don't match. If UTTID is ignored, ",
+	"then files should match not only in line count, but also line ",
+	"by line.\n"
+	    unless defined($hyp_utt);
+    } else {
+	$hyp_utt = $hyphash{$ref_uttid};
+	if (!defined($hyp_utt)) {
+	    next unless $RefHypMatch;
+	    die "UTTID '$ref_uttid' from the reference file '$ref' (line $.) ",
+	    "could not be found in the hypothesis file '$hyp'.\n";
+	}
     }
 
-    ($hyp_utt,$hyp_uttid)=s3_magic_norm($hyp_utt);
+    # Normalize hypothesis utterance
+    ($hyp_utt, $hyp_uttid) = s3_magic_norm($hyp_utt);
 
-    if(! defined $IgnoreUttID){
-	die "Utterance ID mismatch on line $.: $ref_uttid != $hyp_uttid"
-	    unless $ref_uttid eq $hyp_uttid;
-    }
+    # Redundant: if we are here after the above check, then uttid matches.
+    # if(! $IgnoreUttID) {
+    # 	die "Utterance ID mismatch on line $.: $ref_uttid != $hyp_uttid"
+    # 	    unless $ref_uttid eq $hyp_uttid;
+    # }
 
     # Split the text into an array of words
     my @ref_words = split ' ', $ref_utt;
@@ -77,15 +105,18 @@ while (defined(my $ref_utt = <REF>)) {
 	@hyp_words = map { split "" } @hyp_words;
     }
 
-    my (@align_matrix, @backtrace_matrix);
-
     # Initialize the alignment and backtrace matrices
+    my (@align_matrix, @backtrace_matrix);
     initialize(\@ref_words, \@hyp_words, \@align_matrix, \@backtrace_matrix);
+
     # Do DP alignment maintaining backtrace pointers
-    my $cost = align(\@ref_words, \@hyp_words, \@align_matrix, \@backtrace_matrix);
+    my $cost = align(\@ref_words, \@hyp_words,
+		     \@align_matrix, \@backtrace_matrix);
+
     # Find the backtrace
-    my ($alignment, $ins, $del, $subst, $match) = backtrace(\@ref_words, \@hyp_words,
-							    \@align_matrix, \@backtrace_matrix);
+    my ($alignment, $ins, $del, $subst, $match) =
+	backtrace(\@ref_words, \@hyp_words,
+		  \@align_matrix, \@backtrace_matrix);
 
     # Format the alignment nicely
     my ($ref_align, $hyp_align) = ("", "");
@@ -98,10 +129,11 @@ while (defined(my $ref_utt = <REF>)) {
 		($ref =~ /\p{InCJKUnifiedIdeographs}/ or
 		 $ref =~ /\p{Han}/ or
 		 $hyp =~ /\p{Han}/)) {
-		# Assume this is Chinese, no capitalization so put ** around errors
+		# Assume this is Chinese, no capitalization
+		# so put ** around errors
 		if ($ref ne $hyp) {
 		    $ref = "*$ref*";
-	    	    $hyp = "*$hyp*";
+		    $hyp = "*$hyp*";
 		}
 	    } elsif ($ref eq $hyp) {
 		# Capitalize errors (they already are...), lowercase matches
@@ -125,38 +157,51 @@ while (defined(my $ref_utt = <REF>)) {
     print "$ref_align ($ref_uttid)\n$hyp_align ($hyp_uttid)\n";
 
     # Print out the word error and accuracy rates
-    my $error = @ref_words == 0 ? 1 : $cost/@ref_words;
     my $acc = @ref_words == 0 ? 0 : $match/@ref_words;
-    printf("Words: %d Correct: %d Errors: %d Percent correct = %.2f%% Error = %.2f%% Accuracy = %.2f%%\n",
-	   scalar(@ref_words), $match, $cost, $acc*100, $error*100, 100-$error*100);
+    my $error = @ref_words == 0 ? 1 : $cost/@ref_words;
+    printf("Words: %d Correct: %d Errors: %d Percent correct = %.2f%% " .
+	   "Error = %.2f%% Accuracy = %.2f%%\n",
+	   scalar(@ref_words), $match, $cost, $acc*100,
+	   $error*100, 100-$error*100);
     print "Insertions: $ins Deletions: $del Substitutions: $subst\n";
 
     $total_cost += $cost;
-    $total_match += $match;
-    $total_words += @ref_words;
-    $total_hyp += @hyp_words;
     $total_ins += $ins;
     $total_del += $del;
     $total_subst += $subst;
+    $total_match += $match;
+    $total_hyp += @hyp_words;
+    $total_words += @ref_words;
 }
-# Print out the total word error and accuracy rates
-my ($error, $acc);
-if ($total_words == 0) {
-    $error = $total_cost/$total_hyp;
-    $acc = $total_match/$total_hyp;
-}
-else {
-    $error = $total_cost/$total_words;
-    $acc = $total_match/$total_words;
-}
-printf("TOTAL Words: %d Correct: %d Errors: %d\nTOTAL Percent correct = %.2f%% Error = %.2f%% Accuracy = %.2f%%\n",
-       $total_words, $total_match, $total_cost, $acc*100, $error*100, 100-$error*100);
-print "TOTAL Insertions: $total_ins Deletions: $total_del Substitutions: $total_subst\n";
 
-# This function normalizes a line of a match file. 
+# Print out the total word error and accuracy rates
+my ($acc, $error);
+if ($total_words == 0) {
+    $acc = $total_match/$total_hyp;
+    $error = $total_cost/$total_hyp;
+} else {
+    $acc = $total_match/$total_words;
+    $error = $total_cost/$total_words;
+}
+printf("TOTAL Words: %d Correct: %d Errors: %d\n" .
+       "TOTAL Percent correct = %.2f%% Error = %.2f%% Accuracy = %.2f%%\n",
+       $total_words, $total_match, $total_cost,
+       $acc*100, $error*100, 100-$error*100);
+print("TOTAL Insertions: $total_ins Deletions: $total_del ",
+      "Substitutions: $total_subst\n");
+
+# This function normalizes a line of a match file.
 sub s3_magic_norm{
     my ($word)=@_;
     my $uttid;
+
+    # Skip empty lines
+    return undef if $word =~ /^\s*$/;
+
+    # Skip commented lines
+    if ($SkipComments) {
+	return undef if $word =~ /^\s*#/;
+    }
 
     # Remove line endings
     $word =~ s/[\n\r]+$//;  # the agnostic way...
@@ -166,7 +211,7 @@ sub s3_magic_norm{
     $uttid = $1;
 
     # Normalize case
-    $word = uc $word;   
+    $word = uc $word;
     # Remove filler words and context cues
     $word =~ s/<[^>]+>//g;
     $word =~ s/\+\+[^+]+\+\+//g;
@@ -181,7 +226,7 @@ sub s3_magic_norm{
     # Split apart compound words and acronyms
     $word =~ tr/-_./  /;
 
-    return ($word,$uttid);
+    return ($word, $uttid);
 }
 
 sub initialize {
@@ -218,7 +263,8 @@ sub align {
 	    $ins = $$align_matrix[$i][$j-1] + 1;
 	    $del = $$align_matrix[$i-1][$j] + 1;
 	    $subst = $$align_matrix[$i-1][$j-1] + $cost;
-	    print "Costs at $i $j: INS $ins DEL $del SUBST $subst\n" if $Verbose;
+	    print "Costs at $i $j: INS $ins DEL $del SUBST $subst\n"
+		if $Verbose;
 
 	    # Get the minimum one
 	    my $min = BIG_NUMBER;
@@ -232,17 +278,18 @@ sub align {
 	    # If the costs are equal, prefer match or substitution
 	    # (keep the path diagonal).
 	    if ($min == $subst) {
+		$$backtrace_matrix[$i][$j] = MATCH + $cost;
 		print(($cost ? "SUBSTITUTION" : "MATCH"),
-		      "($$ref_words[$i-1] <=> $$hyp_words[$j-1])\n") if $Verbose;
-		$$backtrace_matrix[$i][$j] = MATCH+$cost;
+		      "($$ref_words[$i-1] <=> $$hyp_words[$j-1])\n")
+		    if $Verbose;
 	    }
 	    elsif ($min == $ins) {
-		print "INSERTION (0 => $$hyp_words[$j-1])\n" if $Verbose;
 		$$backtrace_matrix[$i][$j] = INS;
+		print "INSERTION (0 => $$hyp_words[$j-1])\n" if $Verbose;
 	    }
 	    elsif ($min == $del) {
-		print "DELETION ($$ref_words[$i-1] => 0)\n" if $Verbose;
 		$$backtrace_matrix[$i][$j] = DEL;
+		print "DELETION ($$ref_words[$i-1] => 0)\n" if $Verbose;
 	    }
 	}
     }
@@ -256,11 +303,11 @@ sub backtrace {
     my @alignment;
     my $i = @$ref_words;
     my $j = @$hyp_words;
-    my ($inspen, $delpen, $substpen, $match) = (0,0,0,0);
+    my ($inspen, $delpen, $substpen, $match) = (0, 0, 0, 0);
+
     while (!($i == 0 and $j == 0)) {
 	my $pointer = $$backtrace_matrix[$i][$j];
-	print "Cost at $i $j: $$align_matrix[$i][$j]\n"
-	    if $Verbose;
+	print "Cost at $i $j: $$align_matrix[$i][$j]\n" if $Verbose;
 	if ($pointer == INS) {
 	    print "INSERTION (0 => $$hyp_words[$j-1])" if $Verbose;
 	    # Append the pair 0:hyp[j] to the front of the alignment
@@ -287,7 +334,8 @@ sub backtrace {
 	    print " - moving to $i $j\n" if $Verbose;
 	}
 	elsif ($pointer == SUBST) {
-	    print "SUBSTITUTION ($$ref_words[$i-1] <=> $$hyp_words[$j-1])" if $Verbose;
+	    print "SUBSTITUTION ($$ref_words[$i-1] <=> $$hyp_words[$j-1])"
+		if $Verbose;
 	    # Append the pair ref[i]:hyp[j] to the front of the alignment
 	    unshift @alignment, [$$ref_words[$i-1], $$hyp_words[$j-1]];
 	    ++$substpen;
@@ -307,11 +355,12 @@ __END__
 
 =head1 NAME
 
-calculate_wer - Calculate Word Error Rate from a reference and hypothesis file
+word_align - Align reference and hypothesis files and calculate
+recognition accuracy.
 
 =head1 SYNOPSIS
 
- calculate_wer [options] reference_file hypothesis_file
+ word_align [options] reference_file hypothesis_file
 
 =head1 OPTIONS
 
@@ -324,5 +373,31 @@ Print a brief help message and exit.
 =item B<--verbose>, B<-v>
 
 Print out messages tracing the alignment algorithm.
+
+=item B<--ignore-uttid>, B<-i>
+
+Ignore utterance ID when aligning. This option requires that
+reference and hypothesis files should strictly match line by line.
+
+=item B<--cer>, B<-c>
+
+Character based alignment; compute CER (Character Error Rate).
+
+=item B<--comments>, B<--no-comments>
+
+Specifies whenether reference file contains # commented lines which
+should be ignored. B<Default>: --no-comments (read all). Useful during
+sphinx_batch testing, when corresponing control and reference/transcription
+file lines can be handily commented (and are ignored by the decoder, other
+processing tools) and then they should also be ignored by this tool.
+
+=item B<--ref-hyp-match>, B<--no-ref-hyp-match>
+
+Specifies whenether reference and hypothesis files should match.
+B<Default>: --ref-hyp-match (should  match). Non-strict matching is useful
+during sphinx_batch partial tests, when hypothesis file contains only part
+of original transcription/reference utterances. The opposite still holds:
+all utterances of hypothesis file should have a corresponding match in
+reference file.
 
 =cut
