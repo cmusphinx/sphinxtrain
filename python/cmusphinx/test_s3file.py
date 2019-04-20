441ceb2eff0ec985bfa2daaab5dfeb725c855d02
Taylor Smock <smocktaylor@gmail.com>
2019-04-20 06:34:58 -0600

    Python3: Convert asserts to newer format
    Signed-off-by: Taylor Smock <smocktaylor@gmail.com>

--- python/cmusphinx/test_s3file.py
+++ python/cmusphinx/test_s3file.py
@@ -12,7 +12,7 @@ class TestS3File(unittest.TestCase):
         s3file.S3File_write("tmp.1d").write1d(data)
         data2 = s3file.S3File("tmp.1d").read1d()
         # Assert that all elements are equal
-        self.assertEquals(sum(data == data2), len(data))
+        self.assertEqual(sum(data == data2), len(data))
         os.unlink("tmp.1d")
 
     def test_2dfile(self):
@@ -20,7 +20,7 @@ class TestS3File(unittest.TestCase):
         s3file.S3File_write("tmp.2d").write2d(data)
         data2 = s3file.S3File("tmp.2d").read2d()
         # Assert that all elements are equal
-        self.assertEquals((data == data2).sum(), data.size)
+        self.assertEqual((data == data2).sum(), data.size)
         os.unlink("tmp.2d")
 
     def test_3dfile(self):
@@ -28,7 +28,7 @@ class TestS3File(unittest.TestCase):
         s3file.S3File_write("tmp.3d").write3d(data)
         data2 = s3file.S3File("tmp.3d").read3d()
         # Assert that all elements are equal
-        self.assertEquals((data == data2).sum(), data.size)
+        self.assertEqual((data == data2).sum(), data.size)
         os.unlink("tmp.3d")
 
 if __name__ == '__main__':
--- python/cmusphinx/test_s3model.py
+++ python/cmusphinx/test_s3model.py
@@ -15,10 +15,10 @@ class TestS3Model(unittest.TestCase):
         self.acmod = s3model.S3Model(self.testdir)
 
     def test_created(self):
-        self.assert_(abs(self.acmod.tmat[0][0,1] - 0.3326) < 0.01)
-        self.assert_(abs(sum(self.acmod.mixw[0,0]) - 1.0) < 0.01)
-        self.assert_(abs(self.acmod.var[0][0][0][0] - 0.2583) < 0.01)
-        self.assert_(abs(self.acmod.var[35][0][0][38] - 16.9266) < 0.01)
+        self.assertTrue(abs(self.acmod.tmat[0][0,1] - 0.3326) < 0.01)
+        self.assertTrue(abs(sum(self.acmod.mixw[0,0]) - 1.0) < 0.01)
+        self.assertTrue(abs(self.acmod.var[0][0][0][0] - 0.2583) < 0.01)
+        self.assertTrue(abs(self.acmod.var[35][0][0][38] - 16.9266) < 0.01)
 
     def test_compute(self):
         mfcc = s2mfc.open(os.path.join(self.testdir, 'man.ah.111a.mfc')).getall()
@@ -28,7 +28,7 @@ class TestS3Model(unittest.TestCase):
         expected = [3.03518949e-36, 1.00000000e+00, 4.47046728e-16, 1.07179724e-01]
         senscr = self.acmod.senone_compute(senones, feat[0])
         for i,s in enumerate(senones):
-            self.assert_(abs(senscr[s] - expected[i]) < 0.01)
+            self.assertTrue(abs(senscr[s] - expected[i]) < 0.01)
 
 if __name__ == '__main__':
     unittest.main()
