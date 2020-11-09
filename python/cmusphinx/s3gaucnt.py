53c296230a81c95e701bc6781169eaa496b75928
David Huggins-Daines <dhdaines@gmail.com>
2020-11-09 17:19:45 -0500

    port to sphinx3 and fix formatting, hope it works

--- python/cmusphinx/s3gaucnt.py
+++ python/cmusphinx/s3gaucnt.py
@@ -2,7 +2,6 @@
 #
 # You may copy and modify this freely under the same terms as
 # Sphinx-III
-
 """Read/write Sphinx-III Gaussian parameter count files.
 
 This module reads and writes the expected Gaussian mixture occupancy
@@ -10,19 +9,21 @@ count files created by SphinxTrain's implementation of the
 Forward-Backward algorithm for training (semi-)continuous HMMs.
 """
 
-__author__ = "David Huggins-Daines <dhuggins@cs.cmu.edu>"
+__author__ = "David Huggins-Daines <dhdaines@gmail.com>"
 __version__ = "$Revision$"
 
-from struct import unpack, pack
-from numpy import array, reshape, shape, fromstring
-from s3file import S3File
+from struct import unpack
+from numpy import reshape, frombuffer
+from .s3file import S3File
 import os
 
-def open(filename, mode="rb", attr={"version":1.0}):
+
+def open(filename, mode="rb", attr={"version": 1.0}):
     if mode in ("r", "rb"):
-        return S3GauCntFile(filename, mode)
+        return S3GauCntFile(filename)
     else:
-        raise Exception, "mode must be 'r' or 'rb'"
+        raise Exception("mode must be 'r' or 'rb'")
+
 
 def accumdirs(accumdirs):
     "Read and accumulate counts from several directories"
@@ -30,10 +31,10 @@ def accumdirs(accumdirs):
     for d in accumdirs:
         try:
             subgau = S3GauCntFile(os.path.join(d, "gauden_counts"), "rb")
-        except:
+        except OSError:
             subgau = None
             continue
-        if gauden == None:
+        if gauden is None:
             gauden = subgau
         else:
             for m, mgau in enumerate(gauden.mean):
@@ -42,6 +43,7 @@ def accumdirs(accumdirs):
                     gauden.var[m][f] += subgau.var[m][f]
                     gauden.dnom[m][f] += subgau.dnom[m][f]
     return gauden
+
 
 def accumdirs_full(accumdirs):
     "Read and accumulate full-covariance counts from several directories"
@@ -49,10 +51,10 @@ def accumdirs_full(accumdirs):
     for d in accumdirs:
         try:
             subgau = S3FullGauCntFile(os.path.join(d, "gauden_counts"), "rb")
-        except:
+        except OSError:
             subgau = None
             continue
-        if gauden == None:
+        if gauden is None:
             gauden = subgau
         else:
             for m, mgau in enumerate(gauden.mean):
@@ -62,22 +64,25 @@ def accumdirs_full(accumdirs):
                     gauden.dnom[m][f] += subgau.dnom[m][f]
     return gauden
 
-def open_full(filename, mode="rb", attr={"version":1.0}):
+
+def open_full(filename, mode="rb", attr={"version": 1.0}):
     if mode in ("r", "rb"):
         return S3FullGauCntFile(filename, mode)
     else:
-        raise Exception, "mode must be 'r', 'rb'"
+        raise Exception("mode must be 'r', 'rb'")
+
 
 class S3GauCntFile(S3File):
     "Read Sphinx-III format Gaussian count files"
+
     def __init__(self, file, mode):
         S3File.__init__(self, file, mode)
         self._load()
 
     def readgauheader(self):
         if self.fileattr["version"] != "1.0":
-            raise Exception("Version mismatch: must be 1.0 but is "
-                            + self.fileattr["version"])
+            raise Exception("Version mismatch: must be 1.0 but is " +
+                            self.fileattr["version"])
         self.fh.seek(self.data_start, 0)
         self.has_mean = unpack(self.swap + "I", self.fh.read(4))[0]
         self.has_var = unpack(self.swap + "I", self.fh.read(4))[0]
@@ -100,14 +105,13 @@ class S3GauCntFile(S3File):
     def _loadgau(self):
         self._nfloats = unpack(self.swap + "I", self.fh.read(4))[0]
         if self._nfloats != self.n_mgau * self.density * self.blk:
-            raise Exception(("Number of data points %d doesn't match "
-                             + "total %d = %d*%d*%d")
-                            %
-                            (self._nfloats,
-                             self.n_mgau * self.density * self.blk,
-                             self.n_mgau, self.density, self.blk))
+            raise Exception(
+                ("Number of data points %d doesn't match " +
+                 "total %d = %d*%d*%d") %
+                (self._nfloats, self.n_mgau * self.density * self.blk,
+                 self.n_mgau, self.density, self.blk))
         spam = self.fh.read(self._nfloats * 4)
-        data = fromstring(spam, 'f')
+        data = frombuffer(spam, 'f').copy()
         if self.otherend:
             data = data.byteswap()
         params = []
@@ -116,14 +120,16 @@ class S3GauCntFile(S3File):
             mgau = []
             params.append(mgau)
             for j in range(0, self.n_feat):
-                rnext = r + self.density * self.veclen[j];
+                rnext = r + self.density * self.veclen[j]
                 gmm = reshape(data[r:rnext], (self.density, self.veclen[j]))
                 mgau.append(gmm)
                 r = rnext
-        return params        
+        return params
+
 
 class S3FullGauCntFile(S3GauCntFile):
     "Read Sphinx-III format Gaussian full covariance matrix files"
+
     def _load(self):
         self.readgauheader()
         if self.has_mean:
@@ -135,14 +141,13 @@ class S3FullGauCntFile(S3GauCntFile):
     def _loadfullgau(self):
         self._nfloats = unpack(self.swap + "I", self.fh.read(4))[0]
         if self._nfloats != self.n_mgau * self.density * self.blk * self.blk:
-            raise Exception(("Number of data points %d doesn't match "
-                             + "total %d = %d*%d*%d*%d")
-                            %
-                            (self._nfloats,
-                             self.n_mgau * self.density * self.blk * self.blk,
-                             self.n_mgau, self.density, self.blk, self.blk))
+            raise Exception(
+                ("Number of data points %d doesn't match " +
+                 "total %d = %d*%d*%d*%d") %
+                (self._nfloats, self.n_mgau * self.density * self.blk *
+                 self.blk, self.n_mgau, self.density, self.blk, self.blk))
         spam = self.fh.read(self._nfloats * 4)
-        data = fromstring(spam, 'f')
+        data = frombuffer(spam, 'f').copy()
         if self.otherend:
             data = data.byteswap()
         params = []
@@ -151,10 +156,9 @@ class S3FullGauCntFile(S3GauCntFile):
             mgau = []
             params.append(mgau)
             for j in range(0, self.n_feat):
-                rnext = r + self.density * self.veclen[j] * self.veclen[j];
-                gmm = reshape(data[r:rnext], (self.density,
-                                              self.veclen[j],
-                                              self.veclen[j]))
+                rnext = r + self.density * self.veclen[j] * self.veclen[j]
+                gmm = reshape(data[r:rnext],
+                              (self.density, self.veclen[j], self.veclen[j]))
                 mgau.append(gmm)
                 r = rnext
         return params
