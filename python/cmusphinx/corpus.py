3b430839f73eeb625be9ff8121fa167c89bf217e
David Huggins-Daines <dhdaines@gmail.com>
2020-11-09 17:07:08 -0500

    port to sphinx3 and fix small problems and formatting

--- python/cmusphinx/corpus.py
+++ python/cmusphinx/corpus.py
@@ -2,17 +2,17 @@
 #
 # You may copy and modify this freely under the same terms as
 # Sphinx-III
-
 """Corpus classes for acoustic model training.
 
 This module provides classes for representing a corpus of utterances
 for acoustic modeling.  The Corpus class implements the iterator
 protocol, acting as a list of Utterance objects.
 """
-__author__ = "David Huggins-Daines <dhuggins@cs.cmu.edu>"
+__author__ = "David Huggins-Daines <dhdaines@gmail.com>"
 __version__ = "$Revision$"
 
 import os
+
 
 class Resource(object):
     """Resource associated with an utterance in a speech corpus.
@@ -23,6 +23,7 @@ class Resource(object):
     """
     pass
 
+
 class FileResourceIterator(object):
     """
     Iterator over items in a FileResource.
@@ -31,17 +32,19 @@ class FileResourceIterator(object):
         self.res = resource
         self.ctl = iter(resource.ctl_file)
 
-    def next(self):
+    def __next__(self):
         # This will raise StopIteration for us at EOF
-        entry = self.ctl.next()
+        entry = next(self.ctl)
         if isinstance(entry, CtlEntry):
-            path = os.path.join(self.res.base_dir, entry.fileid + self.res.file_ext)
+            path = os.path.join(self.res.base_dir,
+                                entry.fileid + self.res.file_ext)
         else:
             path = os.path.join(self.res.base_dir, entry + self.res.file_ext)
         if self.res.data_type:
             return self.res.data_type(path)
         else:
             return path
+
 
 class FileResource(Resource):
     def __init__(self, ctl_file, base_dir, file_ext, data_type=None):
@@ -62,6 +65,7 @@ class FileResource(Resource):
     def __iter__(self):
         return FileResourceIterator(self)
 
+
 class CtlEntry(object):
     """Entry in a control file"""
     def __init__(self, str):
@@ -75,6 +79,7 @@ class CtlEntry(object):
             self.sf = 0
             self.ef = -1
 
+
 class ListResourceIterator(object):
     """
     Iterator over items in a ListResource.
@@ -83,7 +88,12 @@ class ListResourceIterator(object):
         self.fh = open(resource.file_name)
         self.data_type = resource.data_type
 
-    def next(self):
+    def __del__(self):
+        if self.fh is not None:
+            self.fh.close()
+            self.fh = None
+
+    def __next__(self):
         spam = self.fh.readline()
         if spam == "":
             raise StopIteration
@@ -91,6 +101,7 @@ class ListResourceIterator(object):
             return self.data_type(spam.rstrip())
         else:
             return spam.rstrip()
+
 
 class ListResource(Resource):
     """
@@ -103,7 +114,7 @@ class ListResource(Resource):
 
         If no data_type argument is specified, each item in the list
         will be returned as a string.
-        
+
         @param file_name: File to read resource from
         @ptype file_name: string
         @param data_type: Class implementing the data type of each item
@@ -115,6 +126,7 @@ class ListResource(Resource):
     def __iter__(self):
         return ListResourceIterator(self)
 
+
 class CorpusIterator(object):
     """
     Iterator over elements in a Corpus.
@@ -125,20 +137,21 @@ class CorpusIterator(object):
         if npart > 1:
             pass
         else:
-            for k, v in corpus.resources.iteritems():
+            for k, v in corpus.resources.items():
                 self.iters[k] = iter(v)
 
-    def next(self):
+    def __next__(self):
         utt = {}
-        for k,v in self.iters.iteritems():
-            utt[k] = v.next()
+        for k, v in self.iters.items():
+            utt[k] = next(v)
         return utt
+
 
 class Corpus(object):
     """Corpus of speech data."""
     def __init__(self, ctl_file):
         self.ctl = ListResource(ctl_file, CtlEntry)
-        self.resources = { 'ctl' : self.ctl }
+        self.resources = {'ctl': self.ctl}
 
     def __iter__(self):
         return CorpusIterator(self)
--- python/cmusphinx/test_corpus.py
+++ python/cmusphinx/test_corpus.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import corpus
+from cmusphinx import corpus
 import wave
 import unittest
 import os
 import sys
+
 
 class TestFileResource(unittest.TestCase):
     def setUp(self):
@@ -15,36 +16,38 @@ class TestFileResource(unittest.TestCase):
     def test_string(self):
         ctl = corpus.ListResource(self.testfile, corpus.CtlEntry)
         res = iter(corpus.FileResource(ctl, self.testdir, '.wav'))
-        self.assertEqual(res.next(), os.path.join(self.testdir, 'sa1.wav'))
+        self.assertEqual(next(res), os.path.join(self.testdir, 'sa1.wav'))
 
     def test_wave(self):
         ctl = corpus.ListResource(self.testfile, corpus.CtlEntry)
         res = iter(corpus.FileResource(ctl, self.testdir, '.wav', wave.Wave_read))
-        wav = res.next()
+        wav = next(res)
         self.assertEqual(wav.getnframes(), 45261)
+
 
 class TestListResource(unittest.TestCase):
     def setUp(self):
         thisdir = os.path.dirname(sys.argv[0])
         self.testfile = os.path.join(thisdir, 'test', 'test.ctl')
-        
+
     def test_string(self):
         res = iter(corpus.ListResource(self.testfile))
-        self.assertEqual(res.next(), 'sa1 1 20 sa1_1_20')
-        self.assertEqual(res.next(), 'sa1 25 35 sa1_25_35')
+        self.assertEqual(next(res), 'sa1 1 20 sa1_1_20')
+        self.assertEqual(next(res), 'sa1 25 35 sa1_25_35')
 
     def test_entry(self):
         res = iter(corpus.ListResource(self.testfile, corpus.CtlEntry))
-        entry = res.next()
+        entry = next(res)
         self.assertEqual(entry.fileid, 'sa1')
         self.assertEqual(entry.sf, 1)
         self.assertEqual(entry.ef, 20)
         self.assertEqual(entry.uttid, 'sa1_1_20')
-        entry = res.next()
+        entry = next(res)
         self.assertEqual(entry.fileid, 'sa1')
         self.assertEqual(entry.sf, 25)
         self.assertEqual(entry.ef, 35)
         self.assertEqual(entry.uttid, 'sa1_25_35')
+
 
 if __name__ == '__main__':
     unittest.main()
